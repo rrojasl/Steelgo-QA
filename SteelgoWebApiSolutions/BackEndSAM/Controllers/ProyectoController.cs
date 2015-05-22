bb41bd915c9697c17a255dd6d8f3f64daef8a17d
DeFi.ManriquezLuis <luis.manriquez@definityfirst.com>
2015-05-22 08:46:53 -0500

    Add example of sample service on backendsam

---
+++ SteelgoWebApiSolutions/BackEndSAM/Controllers/ProyectoController.cs
@@ -0,0 +1,22 @@
+﻿using BackEndSAM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BackEndSAM.Controllers
+{
+    public class ProyectoController : ApiController
+    {
+        // GET api/<controller>
+        public ListObject[] Get()
+        {
+            ListObject[] returnObject = new ListObject[2];
+            returnObject[0] = new ListObject("id1", "value1");
+            returnObject[1] = new ListObject("id2", "value2");
+            return returnObject;
+        }
+    }
+}
