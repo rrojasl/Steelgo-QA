a7aaa129dbb56d2e762ff3dedefb5541fe923775
Paola <paola.morales@definityfirst.com>
2015-05-22 16:52:24 -0500

    Added the labels for translations for login page

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/translating/dictionary.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/translating/dictionary.js
@@ -3,15 +3,48 @@
     "en-US": 1
 }
 
-_dictionary = {
+_dictionary = {    
+    //Login Page Labels
+    "loginLabel0001": {
+        "es-MX": "Inicio de Sesión",
+        "en-US": "Login"
+    },
+    "loginLabel0002": {
+        "es-MX": "Usuario",
+        "en-US": "Username"
+    },
+    "loginLabel0003": {
+        "es-MX": "Contraseña",
+        "en-US": "Password"
+    },
+    "loginLabel0004": {
+        "es-MX": "¿Olvidaste tu contraseña?",
+        "en-US": "Forgot your password?"
+    },
+    "loginLabel0005": {
+        "es-MX": "SAM CONTROL by STEELGO, Todos los derechos reservados.",
+        "en-US": "SAM CONTROL by STEELGO, Copyright."
+    },
+    "loginLabel0006": {
+        "es-MX": "Contacto",
+        "en-US": "Contact us"
+    },
+    "loginLabel0007": {
+        "es-MX": "¿Quiénes Somos?",
+        "en-US": "Who we are?"
+    },
+    "loginLabel0008": {
+        "es-MX": "Servicios",
+        "en-US": "Services"
+    },
+    "loginLabel0009": {
+        "es-MX": "Iniciar Sesión",
+        "en-US": "Log In"
+    },
     //Layout Page Labels
     "layoutLabel0001": {
         "es-MX": "Bienvenido",
         "en-US": "Welcome"
-    },
-    "layoutLabel0002": {
-        "es-MX": "Derechos Reservados STEELGO",
-        "en-US": "Copyright STEELGO"
     },
     //Home Partial View Labels
     "homeLabel0001": {
