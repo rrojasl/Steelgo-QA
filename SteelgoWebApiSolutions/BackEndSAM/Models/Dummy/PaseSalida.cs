f1beb04f4d821a539fac16eb3266adb4fcbef493
jemiresq <jonathan.miranda@definityfirst.com>
2015-06-01 08:17:44 -0500

    Aviso Llegada Material

---
+++ SteelgoWebApiSolutions/BackEndSAM/Models/Dummy/PaseSalida.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSAM.Models
+{
+    public class PaseSalida
+    {
+        public int PaseEnviado { get; set; }
+        public List<Files> Archivos { get; set; }
+    }
+}
