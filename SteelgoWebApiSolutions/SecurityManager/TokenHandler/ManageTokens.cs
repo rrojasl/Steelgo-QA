f5067b36ffc2f636a31d0c03a3a2021c55f1f6f5
jhernandezt <julian.hernandez@definityfirst.com>
2015-06-02 13:31:41 -0500

    security

---
+++ SteelgoWebApiSolutions/SecurityManager/TokenHandler/ManageTokens.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.IdentityModel;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Web;
+using JWT;
+using System.Runtime.Remoting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel;
+using System.Configuration;
+
+namespace SecurityManager.TokenHandler
+{
+    public class ManageTokens
+    {
+         private static readonly object _mutex = new object();
+        private static ManageTokens _instance;
+
+        /// <summary>
+        /// constructor privado para implementar el patron Singleton
+        /// </summary>
+        private ManageTokens()
+        {
+        }
+
+        /// <summary>
+        /// crea una instancia de la clase
+        /// </summary>
+        public static ManageTokens Instance
+        {
+            get
+            {
+                lock (_mutex)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ManageTokens();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+
+        public string CreateJwtToken(string userName, string password, string profile)
+        {
+            DateTime unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            double now = Math.Round((DateTime.UtcNow.AddMinutes(30) - unixEpoch).TotalSeconds);
+            //double now = Math.Round((DateTime.UtcNow - unixEpoch).TotalSeconds);
+
+            Dictionary<string, object> payload = new Dictionary<string, object>()
+            {
+                { "UserName", userName },
+                { "Password", password },
+                { "Profile", profile},
+                { "exp", now}
+            };
+
+            string token = JWT.JsonWebToken.Encode(payload, ConfigurationManager.AppSettings["scrKey"], JWT.JwtHashAlgorithm.HS256);
+            return token;
+        }
+
+        public bool ValidateToken(string token)
+        {
+            return false;
+        }
+    }
+}
---
+++ SteelgoWebApiSolutions/SecurityManager/TokenHandler/SecurityConstants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace SecurityManager.TokenHandler
+{
+    public class SecurityConstants
+    {
+        public static readonly byte[] KeyForHmacSha256 = new byte[64];
+
+        public static readonly string TokenIssuer = string.Empty;
+
+        public static readonly string TokenAudience = string.Empty;
+
+        public static readonly double TokenLifetimeMinutes = 30;
+
+        static SecurityConstants()
+        {
+            RNGCryptoServiceProvider cryptoProvider = new RNGCryptoServiceProvider();
+            cryptoProvider.GetNonZeroBytes(KeyForHmacSha256);   //Secure enough? Will change on every call. Has to be made a constant.
+
+            TokenIssuer = "issuer"; //What should be a good value here? web api url?
+
+            TokenAudience = "";  //What should be a good value here?
+        }
+    }
+}
