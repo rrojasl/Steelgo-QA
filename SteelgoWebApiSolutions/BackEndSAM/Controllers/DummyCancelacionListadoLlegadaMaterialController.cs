b8788e68a17c0a51daf29c2710f1b6ef2f5b482b
jemiresq <jonathan.miranda@definityfirst.com>
2015-06-09 08:07:43 -0500

    cambios de llegada de material

--- SteelgoWebApiSolutions/BackEndSAM/Controllers/DummyCancelacionListadoLlegadaMaterialController.cs
+++ SteelgoWebApiSolutions/BackEndSAM/Controllers/DummyCancelacionListadoLlegadaMaterialController.cs
@@ -5,9 +5,11 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using BackEndSAM.Models;
+using System.Web.Http.Cors;
 
 namespace BackEndSAM.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class DummyCancelacionListadoLlegadaMaterialController : ApiController
     {
         // GET api/dummycancelacionlistadollegadamaterial
