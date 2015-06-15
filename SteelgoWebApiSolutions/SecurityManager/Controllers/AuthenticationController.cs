52c68e2f88faa785c7e51b9fb78366357fe3d411
DeFi.ManriquezLuis <luis.manriquez@definityfirst.com>
2015-06-15 10:40:03 -0500

    Merge

--- SteelgoWebApiSolutions/SecurityManager/Controllers/AuthenticationController.cs
+++ SteelgoWebApiSolutions/SecurityManager/Controllers/AuthenticationController.cs
@@ -134,21 +134,12 @@ namespace SecurityManager
         /// </param>
         /// <returns> TransactionalInformation Object in a JSON Response  </returns>
         /// <example> GET securitymanager/api/authentication/ </example>
-        //public TransactionalInformation Delete(string username, string token)
-        //{
-        //    username = dataSecurity.Decode(username);
-        //    token = dataSecurity.Decode(token);
-
-        //    //Create a generic return object
-        //    TransactionalInformation transaction = new TransactionalInformation();
-        //    transaction.IsAuthenicated = true;
-
-        //    if (username == "admin" && token == "EsteEsUnTokenGeneradoDeAlgunaManera")
-        //    {
-        //        transaction.IsAuthenicated = false;
-        //    }
-        //    transaction.IsAuthenicated = false;
-        //    return transaction;
-        //}
+        public TransactionalInformation Delete(string username, string token)
+        {
+            //Create a generic return object
+            TransactionalInformation transaction = new TransactionalInformation();
+            transaction.IsAuthenicated = false;
+            return transaction;
+        }
     }
 }
