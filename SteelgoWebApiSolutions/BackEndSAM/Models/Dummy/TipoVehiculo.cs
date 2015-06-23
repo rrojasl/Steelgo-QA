73c678f31f3f3d2bbdb441dfea637506f19d34e9
jemiresq <jonathan.miranda@definityfirst.com>
2015-06-23 08:33:25 -0500

    tipo de vehiculo

---
+++ SteelgoWebApiSolutions/BackEndSAM/Models/Dummy/TipoVehiculo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSAM.Models
+{
+    public class TipoVehiculo
+    {
+        public string TipoVehiculoID { get; set; }
+        public string Nombre { get; set; }
+    }
+}
