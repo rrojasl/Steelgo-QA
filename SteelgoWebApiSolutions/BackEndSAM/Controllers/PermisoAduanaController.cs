de1e269efb6d38d80ae80b844aaf5602d8db36a9
danielaZC <daniela_pzc@hotmail.com>
2015-06-23 09:54:52 -0500

    cambios Permiso Aduana

--- SteelgoWebApiSolutions/BackEndSAM/Controllers/PermisoAduanaController.cs
+++ SteelgoWebApiSolutions/BackEndSAM/Controllers/PermisoAduanaController.cs
@@ -27,36 +27,49 @@ namespace BackEndSAM.Controllers
         // GET api/<controller>/5
         public object Get(int folio, string token)
         {
-            string payload = "";
-            string newToken = "";
-            bool tokenValido = ManageTokens.Instance.ValidateToken(token, out payload, out newToken);
-            if (tokenValido)
-            {
-                JavaScriptSerializer serializer = new JavaScriptSerializer();
-                Sam3_Usuario usuario = serializer.Deserialize<Sam3_Usuario>(payload);
+              string payload = "";
+              string newToken = "";
+              bool tokenValido = ManageTokens.Instance.ValidateToken(token, out payload, out newToken);
+              if (tokenValido)
+              {
+                  JavaScriptSerializer serializer = new JavaScriptSerializer();
+                  Sam3_Usuario usuario = serializer.Deserialize<Sam3_Usuario>(payload);
+ 
+                object listaDatosAduana = permiso.ObtenerDatosAvisoLlegada(folio);
+                object planas = permiso.ObtenerPlanas(folio);
+                object proyectos = permiso.ObtenerProyectos(folio);
+                permiso.InsertarPermisoADuana(folio, usuario);
 
-                List<BackEndSAM.Models.FormatoPermisoAduana> listaDatosAduana = permiso.ObtenerDatosAvisoLlegada(folio);
-                List<string> planas = permiso.ObtenerPlanas(folio);
-                List<string> proyectos = permiso.ObtenerProyectos(folio);
-
-                permiso.EnviarCorreo(listaDatosAduana, planas, proyectos);
+               //permiso.EnviarCorreo(listaDatosAduana, planas, proyectos);
                return permiso.InsertarPermisoADuana(folio, usuario);
                 //return listaDatosAduana;
-            }
-            else
-            {
-                TransactionalInformation result = new TransactionalInformation();
-                result.ReturnMessage.Add(payload);
-                result.ReturnCode = 401;
-                result.ReturnStatus = false;
-                result.IsAuthenicated = false;
-                return result;
-            }
+              }
+              else
+              {
+                  TransactionalInformation result = new TransactionalInformation();
+                  result.ReturnMessage.Add(payload);
+                  result.ReturnCode = 401;
+                  result.ReturnStatus = false;
+                  result.IsAuthenicated = false;
+                  return result;
+              }
+      
         }
 
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public void Post(string nombre, string extension, int folio)
         {
+            //Sam3_PermisoAduana permisoAduana =  new Sam3_PermisoAduana();
+            //if (permisoAduana.PermisoAduanaID == null)
+            //{
+            //    //permiso.GuardarDocumento();
+            //    //permiso.GuardarDatosPermisoAutorizado();
+            //}
+            //else
+            //{
+            //    permiso.ActualizarDocumento();
+            //    permiso.ActualizarDatosPermisoAutorizado();
+            //}
         }
 
         // PUT api/<controller>/5
--- SteelgoWebApiSolutions/BackEndSAM/DataAcces/PermisoAduana.cs
+++ SteelgoWebApiSolutions/BackEndSAM/DataAcces/PermisoAduana.cs
@@ -17,27 +17,40 @@ namespace BackEndSAM.DataAcces
         /// </summary>
         /// <param name="folio">Folio seleccionado</param>
         /// <returns>Lista de Formato Permiso aduana</returns>
-        public List<FormatoPermisoAduana> ObtenerDatosAvisoLlegada(int folio)
+        public object ObtenerDatosAvisoLlegada(int folio)
         {
-            List<FormatoPermisoAduana> listaAvisoLlegada = new List<FormatoPermisoAduana>();
-            using (SamContext ctx = new SamContext())
+            try
             {
-                listaAvisoLlegada = (from x in ctx.Sam3_FolioAvisoLlegada
-                                     join p in ctx.Sam3_Proveedor on x.ProveedorID equals p.ProveedorID
-                                     join ch in ctx.Sam3_Chofer on x.ChoferID equals ch.ChoferID
-                                     join tr in ctx.Sam3_Transportista on x.TransportistaID equals tr.TransportistaID
-                                     join ca in ctx.Sam3_Camion on x.CamionID equals ca.CamionID
-                                     where (x.FolioAvisoLlegadaID == folio)
-                                     select new FormatoPermisoAduana
-                                     {
-                                         FechaRecepcion = x.FechaRecepcion,
-                                         NombreProveedor = p.Nombre,
-                                         NombreChofer = ch.Nombre,
-                                         NombreTransportista = tr.Nombre,
-                                         PlacasCamion = ca.Placas
-                                     }).ToList();
+                List<FormatoPermisoAduana> listaAvisoLlegada = new List<FormatoPermisoAduana>();
+                using (SamContext ctx = new SamContext())
+                {
+                    listaAvisoLlegada = (from x in ctx.Sam3_FolioAvisoLlegada
+                                         join p in ctx.Sam3_Proveedor on x.ProveedorID equals p.ProveedorID
+                                         join ch in ctx.Sam3_Chofer on x.ChoferID equals ch.ChoferID
+                                         join tr in ctx.Sam3_Transportista on x.TransportistaID equals tr.TransportistaID
+                                         join ca in ctx.Sam3_Camion on x.CamionID equals ca.CamionID
+                                         where (x.FolioAvisoLlegadaID == folio)
+                                         select new FormatoPermisoAduana
+                                         {
+                                             FechaRecepcion = x.FechaRecepcion,
+                                             NombreProveedor = p.Nombre,
+                                             NombreChofer = ch.Nombre,
+                                             NombreTransportista = tr.Nombre,
+                                             PlacasCamion = ca.Placas
+                                         }).ToList();
+                }
+                return listaAvisoLlegada;
             }
-            return listaAvisoLlegada;
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
         }
 
         /// <summary>
@@ -45,17 +58,30 @@ namespace BackEndSAM.DataAcces
         /// </summary>
         /// <param name="folio">Folio seleccionado</param>
         /// <returns>Lista de Planas</returns>
-        public List<string> ObtenerPlanas(int folio)
+        public object ObtenerPlanas(int folio)
         {
-            List<string> listaPlana = new List<string>();
-            using (SamContext ctx = new SamContext())
+            try
             {
-                listaPlana = (from avPlana in ctx.Sam3_Rel_AvisoLlegada_Plana
-                              join plana in ctx.Sam3_Plana on avPlana.PlanaID equals plana.PlanaID
-                              where (avPlana.FolioAvisoLlegadaID == folio)
-                              select plana.Placas).ToList();
+                List<string> listaPlana = new List<string>();
+                using (SamContext ctx = new SamContext())
+                {
+                    listaPlana = (from avPlana in ctx.Sam3_Rel_AvisoLlegada_Plana
+                                  join plana in ctx.Sam3_Plana on avPlana.PlanaID equals plana.PlanaID
+                                  where (avPlana.FolioAvisoLlegadaID == folio)
+                                  select plana.Placas).ToList();
+                }
+                return listaPlana;
             }
-            return listaPlana;
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
         }
 
         /// <summary>
@@ -63,17 +89,31 @@ namespace BackEndSAM.DataAcces
         /// </summary>
         /// <param name="folio">Folio seleccionado</param>
         /// <returns>Lista de Proyectos</returns>
-        public List<string> ObtenerProyectos(int folio)
+        public object ObtenerProyectos(int folio)
         {
-            List<string> listaProyecto = new List<string>();
-            using (SamContext ctx = new SamContext())
+            try
             {
-                listaProyecto = (from avProy in ctx.Sam3_Rel_FolioAvisoLlegada_Proyecto
-                                 join proy in ctx.Sam3_Proyecto on avProy.ProyectoID equals proy.ProyectoID
-                                 where (avProy.FolioAvisoLlegadaID == folio)
-                                 select proy.Nombre).ToList();
+
+                List<string> listaProyecto = new List<string>();
+                using (SamContext ctx = new SamContext())
+                {
+                    listaProyecto = (from avProy in ctx.Sam3_Rel_FolioAvisoLlegada_Proyecto
+                                     join proy in ctx.Sam3_Proyecto on avProy.ProyectoID equals proy.ProyectoID
+                                     where (avProy.FolioAvisoLlegadaID == folio)
+                                     select proy.Nombre).ToList();
+                }
+                return listaProyecto;
             }
-            return listaProyecto;
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
         }
 
         /// <summary>
@@ -82,86 +122,178 @@ namespace BackEndSAM.DataAcces
         /// <param name="listaAvisoLlegada">Lista con los datos del aviso llegada correspondiente al folio</param>
         /// <param name="planas">Lista con las planas correspondientes al folio</param>
         /// <param name="proyectos">Lista con los proyectos correspondientes al folio</param>
-        public void EnviarCorreo(List<FormatoPermisoAduana> listaAvisoLlegada, List<string> planas, List<string> proyectos)
+        //public object EnviarCorreo(object listaAvisoLlegada, object planas, object proyectos)
+        //{
+        //    try
+        //    {
+        //        string listadoProyectos = string.Join(", ", proyectos);
+        //        string fechaRecepcion = listaAvisoLlegada.FechaRecepcion.ToString().Substring(0, 9);
+        //        DateTime nuevaFecha = DateTime.Parse(fechaRecepcion);
+        //        StringBuilder body = new StringBuilder();
+        //        MailMessage mail = new MailMessage();
+        //        SmtpClient SmtpServer = new SmtpClient("smtp.office365.com");
+        //        mail.From = new MailAddress("daniela.zertuche@definityfirst.com");
+        //        mail.To.Add("daniela.zertuche@definityfirst.com");
+        //        mail.Subject = "Nuevo Permiso de Aduana";
+        //        mail.IsBodyHtml = true;
+
+        //        //HTML para las planas
+        //        for (int i = 0; i < planas.Count; i++)
+        //        {
+        //            body.AppendFormat("Plataforma: " + planas[i] + "<br />"
+        //                                 + "Unidad: " + /*Unidad correspondiente a la plana*/ i + "<br />"
+        //                                 + "Modelo: " + /*Modelo correspondiente a la plana*/ i + "<br />");
+        //        }
+
+        //        //HTML del mensaje
+        //        mail.Body = "Buen Dia Capit&aacute;n<br />"
+        //                    + "Solicito permiso de acceso para la siguiente unidad para los d&iacute;as " + String.Format("{0:dd/MM/yyyy}", nuevaFecha.AddDays(-2))
+        //                    + " al " + String.Format("{0:dd/MM/yyyy}", nuevaFecha.AddDays(2))
+        //                    + ", que estar&aacute;  ingresando a las instalaciones de Steelgo.<br /> <br />"
+        //                    + " Cliente: " + listaAvisoLlegada[0].NombreProveedor + "<br />"
+        //                    + "Proyecto: " + listadoProyectos + "<br />"
+        //                    + "Operador: " + listaAvisoLlegada[0].NombreChofer + "<br />"
+        //                    + "L&iacute;nea Transportista: " + listaAvisoLlegada[0].NombreTransportista + "<br />"
+        //                    + "Placas: " + listaAvisoLlegada[0].PlacasCamion + "<br />"
+        //                    + body;
+
+        //        SmtpServer.Port = 587;
+        //        SmtpServer.Credentials = new System.Net.NetworkCredential("user", "password");
+        //        SmtpServer.EnableSsl = true;
+
+        //        SmtpServer.Send(mail);
+
+        //        TransactionalInformation result = new TransactionalInformation();
+        //        result.ReturnMessage.Add("Ok");
+        //        result.ReturnCode = 200;
+        //        result.ReturnStatus = false;
+        //        result.IsAuthenicated = true;
+
+        //        return result;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        TransactionalInformation result = new TransactionalInformation();
+        //        result.ReturnMessage.Add(ex.Message);
+        //        result.ReturnCode = 500;
+        //        result.ReturnStatus = false;
+        //        result.IsAuthenicated = true;
+
+        //        return result;
+        //    }
+        //}
+
+        /// <summary>
+        /// Insertar permiso de aduana en base de datos
+        /// </summary>
+        /// <param name="folio">Folio seleccionado por el usuario</param>
+        /// <param name="usuario">Usuario Actual</param>
+        /// <returns></returns>
+        public object InsertarPermisoADuana(int folio, Sam3_Usuario usuario)
         {
             try
             {
-                string listadoProyectos = string.Join(", ", proyectos);
-                string fechaRecepcion = listaAvisoLlegada[0].FechaRecepcion.ToString().Substring(0, 9);
-                DateTime nuevaFecha = DateTime.Parse(fechaRecepcion);
-                StringBuilder body = new StringBuilder();
-                MailMessage mail = new MailMessage();
-                SmtpClient SmtpServer = new SmtpClient("smtp.office365.com");
-                mail.From = new MailAddress("daniela.zertuche@definityfirst.com");
-                mail.To.Add("daniela.zertuche@definityfirst.com");
-                mail.Subject = "Nuevo Permiso de Aduana";
-                mail.IsBodyHtml = true;
-
-                //HTML para las planas
-                for (int i = 0; i < planas.Count; i++)
+                using (SamContext ctx = new SamContext())
                 {
-                    body.AppendFormat("Plataforma: " + planas[i] + "<br />"
-                                         + "Unidad: " + /*Unidad correspondiente a la plana*/ i + "<br />"
-                                         + "Modelo: " + /*Modelo correspondiente a la plana*/ i + "<br />");
+                    Sam3_PermisoAduana permisoAduana = new Sam3_PermisoAduana();
+                    permisoAduana.FolioAvisoLlegadaID = folio;
+                    permisoAduana.Estatus = "Creado";
+                    permisoAduana.Activo = true;
+                    permisoAduana.UsuarioModificacion = 1; /*usuario.UsuarioID;*/
+                    permisoAduana.FechaModificacion = DateTime.Now;
+                    permisoAduana.PermisoAutorizado = false;
+                    permisoAduana.PermisoTramite = true;
+                    permisoAduana.NumeroPermiso = null;
+                    permisoAduana.FechaGeneracion = DateTime.Now;
+                    permisoAduana.FechaAutorización = null;
+
+                    ctx.Sam3_PermisoAduana.Add(permisoAduana);
+                    ctx.SaveChanges();
+
+                    TransactionalInformation result = new TransactionalInformation();
+                    result.ReturnMessage.Add("Ok");
+                    result.ReturnMessage.Add(permisoAduana.PermisoAduanaID.ToString());
+                    result.ReturnCode = 200;
+                    result.ReturnStatus = true;
+                    result.IsAuthenicated = true;
+
+                    return result;
                 }
-
-                //HTML del mensaje
-                mail.Body = "Buen Dia Capit&aacute;n<br />"
-                            + "Solicito permiso de acceso para la siguiente unidad para los d&iacute;as " + String.Format("{0:dd/MM/yyyy}", nuevaFecha.AddDays(-2))
-                            + " al " + String.Format("{0:dd/MM/yyyy}", nuevaFecha.AddDays(2))
-                            + ", que estar&aacute;  ingresando a las instalaciones de Steelgo.<br /> <br />"
-                            + " Cliente: " + listaAvisoLlegada[0].NombreProveedor + "<br />"
-                            + "Proyecto: " + listadoProyectos + "<br />"
-                            + "Operador: " + listaAvisoLlegada[0].NombreChofer + "<br />"
-                            + "L&iacute;nea Transportista: " + listaAvisoLlegada[0].NombreTransportista + "<br />"
-                            + "Placas: " + listaAvisoLlegada[0].PlacasCamion + "<br />"
-                            + body;
-
-                SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("user", "password");
-                SmtpServer.EnableSsl = true;
-
-                SmtpServer.Send(mail);
             }
             catch (Exception ex)
             {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
             }
         }
 
-       /// <summary>
-       /// Insertar permiso de aduana en base de datos
-       /// </summary>
-       /// <param name="folio">Folio seleccionado por el usuario</param>
-       /// <param name="usuario">Usuario Actual</param>
-       /// <returns></returns>
-        public object InsertarPermisoADuana(int folio, Sam3_Usuario usuario)
+        public object GuardarDocumento(string nombre, string extension, int folio)
         {
-            using (SamContext ctx = new SamContext())
+            try
             {
-                Sam3_PermisoAduana permisoAduana = new Sam3_PermisoAduana();
-                permisoAduana.FolioAvisoLlegadaID = folio;
-                permisoAduana.Estatus = "Creado";
-                permisoAduana.Activo = true;
-                permisoAduana.UsuarioModificacion = usuario.UsuarioID;
-                permisoAduana.FechaModificacion = DateTime.Now;
-                permisoAduana.PermisoAutorizado = false;
-                permisoAduana.PermisoTramite = true;
-                permisoAduana.NumeroPermiso = null;
-                permisoAduana.FechaGeneracion = DateTime.Now;
-                permisoAduana.FechaAutorización = null;
+                int idPermisoAduana = 0;
 
-                ctx.Sam3_PermisoAduana.Add(permisoAduana);
-                ctx.SaveChanges();
+                using (SamContext ctx = new SamContext())
+                {
+                    idPermisoAduana = (from pa in ctx.Sam3_PermisoAduana
+                                       where (pa.FolioAvisoLlegadaID == folio)
+                                       select pa.PermisoAduanaID).First();
+
+                    Sam3_Rel_PermisoAduana_Documento permisoAduana_Documento = new Sam3_Rel_PermisoAduana_Documento();
+                    permisoAduana_Documento.PermisoAduanaID = idPermisoAduana;
+                    permisoAduana_Documento.DocumentoID = 1; /*Documento ID*/
+                    permisoAduana_Documento.Nombre = nombre;
+                    permisoAduana_Documento.Extencion = extension;
+                    permisoAduana_Documento.Activo = true;
+                    permisoAduana_Documento.UsuarioModificacion = 1; /* Usuario Modificacion*/
+                    permisoAduana_Documento.FechaModificacion = DateTime.Now;
+                }
 
                 TransactionalInformation result = new TransactionalInformation();
                 result.ReturnMessage.Add("Ok");
-                result.ReturnMessage.Add(permisoAduana.PermisoAduanaID.ToString());
                 result.ReturnCode = 200;
                 result.ReturnStatus = true;
                 result.IsAuthenicated = true;
 
                 return result;
             }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+                return result;
+            }
+        }
+
+        public void GuardarDatosPermisoAutorizado(int numeroPermiso, int folio)
+        {
+            using (SamContext ctx = new SamContext())
+            {
+                var nuevoPermiso = (from pa in ctx.Sam3_PermisoAduana
+                                    where (pa.FolioAvisoLlegadaID == folio)
+                                    select pa).First();
+
+                nuevoPermiso.PermisoAutorizado = true;
+                nuevoPermiso.PermisoTramite = false;
+                nuevoPermiso.NumeroPermiso = numeroPermiso;
+                nuevoPermiso.FechaAutorización = DateTime.Now;
+            }
+        }
+
+        public void ActualizarDocumento()
+        {
+        }
+
+        public void ActualizarDatosPermisoAutorizado()
+        {
         }
     }
 }
