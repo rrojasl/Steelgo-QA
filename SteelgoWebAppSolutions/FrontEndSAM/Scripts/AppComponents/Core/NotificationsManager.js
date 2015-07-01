5772d8efd7801a6469e794a441cd88cebd1746bf
DeFi.ManriquezLuis <luis.manriquez@definityfirst.com>
2015-07-01 10:33:35 -0500

    Persistent Message Feature

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Core/NotificationsManager.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Core/NotificationsManager.js
@@ -18,27 +18,41 @@
 
 //Method to be called on the document ready and contains all the pertinent code for a partial view
 function notificationsManagertoBeExecutedOnDocumentReady() {
-    //CODE
+    if (Cookies.get("messageBody") != null && Cookies.get("messageComplement") != null && Cookies.get("messageType") != null) {
+        displayMessage(Cookies.get("messageBody"), Cookies.get("messageComplement"), Cookies.get("messageType"))
+    }
 }
 
 /****************************/
 /*    Global Functions      */
 /****************************/
 
+//Method to remove all display classes of the message area
+function cleanDisplayMessage() {
+    $(".alert-container").removeClass("active");
+    $(".message").text("");
+    $(".message").removeClass("success");
+    $(".message").removeClass("warning");
+    $(".message").removeClass("error");
+    Cookies.remove("messageBody", { path: '/' });
+    Cookies.remove("messageComplement", { path: '/' });
+    Cookies.remove("messageType", { path: '/' });
+}
+
 //Method to display a message on the proper area Types(o:success | 1:warning | 2:error)
 function displayMessage(message, messageComplement, type) {
+    cleanDisplayMessage();
     $(".alert-container").addClass("active");
     $(".message").text(message);
+    Cookies.set("messageBody", message, { path: '/' });
+    Cookies.set("messageComplement", messageComplement, { path: '/' });
+    Cookies.set("messageType", type, { path: '/' });
     switch (type) {
         case 0: $(".message").addClass("success"); break;
         case 0: $(".message").addClass("warning"); break;
         case 0: $(".message").addClass("error"); break;
     }
     setTimeout(function () {
-        $(".alert-container").removeClass("active");
-        $(".message").text("");
-        $(".message").removeClass("success");
-        $(".message").removeClass("warning");
-        $(".message").removeClass("error");
+        cleanDisplayMessage();
     }, 5000);
 }
