38cc0740420d723690cce88a8cf112715e283791
DeFi.ManriquezLuis <luis.manriquez@definityfirst.com>
2015-07-01 09:04:19 -0500

    Javascript Base Structure

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/AvisoLlegada/ListadoEspecifico.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/AvisoLlegada/ListadoEspecifico.js
@@ -1,1 +1,28 @@
-﻿
+﻿/********************************************/
+/*********                          *********/
+/*********      Aviso Llegada       *********/
+/*********   Listado Especifico     *********/
+/*********                          *********/
+/********************************************/
+/********************************************/
+
+/****************************/
+/*    Global Variables      */
+/****************************/
+
+//Add all global variables for your partial view here
+
+/****************************/
+/*    Document Ready        */
+/****************************/
+
+//Method to be called on the document ready and contains all the pertinent code for a partial view
+function avisoLlegadaListadoEspecificoToBeExecutedOnDocumentReady() {
+    //CODE
+}
+
+/****************************/
+/*    Global Functions      */
+/****************************/
+
+//Add all your global functions for your partial view here
--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/LlegadaMaterial/ListadoEspecifico.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/LlegadaMaterial/ListadoEspecifico.js
@@ -1,1 +1,28 @@
-﻿
+﻿/********************************************/
+/*********                          *********/
+/*********    Llegada Material      *********/
+/*********   Listado Especifico     *********/
+/*********                          *********/
+/********************************************/
+/********************************************/
+
+/****************************/
+/*    Global Variables      */
+/****************************/
+
+//Add all global variables for your partial view here
+
+/****************************/
+/*    Document Ready        */
+/****************************/
+
+//Method to be called on the document ready and contains all the pertinent code for a partial view
+function llegadaMaterialListadoEspecificoToBeExecutedOnDocumentReady() {
+    //CODE
+}
+
+/****************************/
+/*    Global Functions      */
+/****************************/
+
+//Add all your global functions for your partial view here
