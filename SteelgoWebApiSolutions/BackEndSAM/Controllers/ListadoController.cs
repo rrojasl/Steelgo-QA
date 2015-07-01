d0f98424e2eb3810da99cfd3f5b3a594df2b468c
jhernandezt <julian.hernandez@definityfirst.com>
2015-07-01 12:55:09 -0500

    fix devuelvo código eliminado

--- SteelgoWebApiSolutions/BackEndSAM/Controllers/ListadoController.cs
+++ SteelgoWebApiSolutions/BackEndSAM/Controllers/ListadoController.cs
@@ -18,6 +18,54 @@ namespace BackEndSAM.Controllers
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ListadoController : ApiController
     {
+
+        public object Get(int tipoListado, string token, string parametroBusqueda = "")
+        {
+            string payload = "";
+            string newToken = "";
+            bool tokenValido = ManageTokens.Instance.ValidateToken(token, out payload, out newToken);
+            if (tokenValido)
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                Sam3_Usuario usuario = serializer.Deserialize<Sam3_Usuario>(payload);
+
+                switch (tipoListado)
+                {
+                    case 1: //Folios aviso llegada
+                        return AvisoLlegadaBd.Instance.ObtenerListadoFoliosParaFiltro();
+                    case 2: // Folios de aviso de llegada con permiso de aduana autorizados
+                        return AvisoLlegadaBd.Instance.ObtenerListadoFoliosRequierePermiso();
+                    case 3: // listado de choferes por transportista
+                        return ChoferBd.Instance.ObtenerChoferesProTransportista(Convert.ToInt32(parametroBusqueda), usuario);
+                    case 4: //Obtener cantidades para dashboard
+                        TransactionalInformation rest = new TransactionalInformation();
+                        rest.ReturnMessage.Add("El listado de cantidades de Dashboard requiere de parametros de filtrado");
+                        rest.ReturnCode = 500;
+                        rest.ReturnStatus = false;
+                        rest.IsAuthenicated = false;
+                        return rest;
+                    default:
+                        TransactionalInformation result = new TransactionalInformation();
+                        result.ReturnMessage.Add("Listado no encontrado");
+                        result.ReturnCode = 500;
+                        result.ReturnStatus = false;
+                        result.IsAuthenicated = false;
+                        return result;
+                }
+
+            }
+            else
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(payload);
+                result.ReturnCode = 401;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = false;
+                return result;
+            }
+        }
+
+
         public object Get(string data)
         {
             JavaScriptSerializer serializer = new JavaScriptSerializer();
