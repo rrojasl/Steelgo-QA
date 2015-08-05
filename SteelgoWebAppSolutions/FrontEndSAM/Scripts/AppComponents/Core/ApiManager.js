8513670cd9ac8e0ba641f51d9ff274b21ac8586b
danielaZC <daniela_pzc@hotmail.com>
2015-08-05 18:09:01 -0500

    Edicion, Detalle llegada de material

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Core/ApiManager.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Core/ApiManager.js
@@ -90,7 +90,7 @@ $Camion.add("Tracto");
 $Contacto.add("Contacto");
 $folioEntradaMaterial.add("Listado");
 $FiltrosListadoEntradaMaterial.add("AvisoLlegada");
-$FiltrosListadoLlegadaMaterial.add("FolioAvisoEntrada")
+$FiltrosListadoLlegadaMaterial.add("FolioAvisoEntrada");
 $TipoVehiculo.add("TipoVehiculo");
 $Cliente.add("Cliente");
 $TipoAviso.add("TipoAviso");
