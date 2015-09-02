7a547df955fd7a0051f7b41b3b0c1b8345f282f4
danielaZC <daniela_pzc@hotmail.com>
2015-09-02 10:30:52 -0500

    Guardar colada popup grid

---
+++ SteelgoWebApiSolutions/BackEndSAM/Controllers/ColadaController.cs
@@ -0,0 +1,63 @@
+﻿using BackEndSAM.DataAcces;
+using DatabaseManager.Sam3;
+using SecurityManager.Api.Models;
+using SecurityManager.TokenHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace BackEndSAM.Controllers
+{
+    public class ColadaController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public object Post(string data, string token)
+        {
+            string payload = "";
+            string newToken = "";
+            bool tokenValido = ManageTokens.Instance.ValidateToken(token, out payload, out newToken);
+            if (tokenValido)
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                Sam3_Colada DatosColada = serializer.Deserialize<Sam3_Colada>(data);
+                Sam3_Usuario usuario = serializer.Deserialize<Sam3_Usuario>(payload);
+                return ColadaBd.Instance.GuardarColadaPopUp(DatosColada, usuario);
+            }
+            else
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(payload);
+                result.ReturnCode = 401;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = false;
+                return result;
+            }
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SteelgoWebApiSolutions/BackEndSAM/DataAcces/ColadaBd.cs
@@ -0,0 +1,82 @@
+﻿using DatabaseManager.Sam3;
+using SecurityManager.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSAM.DataAcces
+{
+    public class ColadaBd
+    {
+         private static readonly object _mutex = new object();
+         private static ColadaBd _instance;
+
+        /// <summary>
+        /// constructor privado para implementar el patron Singleton
+        /// </summary>
+         private ColadaBd()
+        {
+        }
+
+        /// <summary>
+        /// crea una instancia de la clase
+        /// </summary>
+         public static ColadaBd Instance
+        {
+            get
+            {
+                lock (_mutex)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ColadaBd();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+
+        public object GuardarColadaPopUp(Sam3_Colada DatosColada, Sam3_Usuario usuario)
+        {
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+                    Sam3_Colada colada = new Sam3_Colada();
+                    colada.FabricanteID = DatosColada.FabricanteID;
+                    colada.AceroID = DatosColada.AceroID;
+                    colada.ProyectoID = DatosColada.ProyectoID;
+                    colada.NumeroColada = DatosColada.NumeroColada;
+                    colada.NumeroCertificado = DatosColada.NumeroCertificado;
+                    colada.HoldCalidad = DatosColada.HoldCalidad;
+                    colada.Activo = true;
+                    colada.UsuarioModificacion = usuario.UsuarioID;
+                    colada.FechaModificacion = DateTime.Now;
+
+                    ctx.Sam3_Colada.Add(colada);
+                    ctx.SaveChanges();
+                }
+
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add("Ok");
+                result.ReturnCode = 200;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+    }
+}
