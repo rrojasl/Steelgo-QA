202df471710630e917635df7ebdd6e3e47d77a75
danielaZC <daniela_pzc@hotmail.com>
2015-09-03 10:10:38 -0500

    editar cuantificacion y bulto

---
+++ SteelgoWebApiSolutions/BackEndSAM/DataAcces/EstatusCuantificacionBd.cs
@@ -0,0 +1,94 @@
+﻿using DatabaseManager.Sam3;
+using SecurityManager.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSAM.DataAcces
+{
+    public class EstatusCuantificacionBd
+    {
+         private static readonly object _mutex = new object();
+         private static EstatusCuantificacionBd _instance;
+
+        /// <summary>
+        /// constructor privado para implementar el patron Singleton
+        /// </summary>
+         private EstatusCuantificacionBd()
+        {
+        }
+
+        /// <summary>
+        /// crea una instancia de la clase
+        /// </summary>
+         public static EstatusCuantificacionBd Instance
+        {
+            get
+            {
+                lock (_mutex)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new EstatusCuantificacionBd();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="FolioCuantificacion"></param>
+        /// <param name="AvisoEntrada"></param>
+        /// <param name="bultoID"></param>
+        /// <param name="usuario"></param>
+        /// <returns></returns>
+         public object CambiarEstatusCuantificacion(int FolioCuantificacion, int AvisoEntrada, int bultoID, Sam3_Usuario usuario)
+         {
+             try
+             {
+                 using (SamContext ctx = new SamContext())
+                 {
+                     if (bultoID != -1)
+                     {
+                         Sam3_Bulto bulto = ctx.Sam3_Bulto.Where(x=>x.FolioCuantificacionID == FolioCuantificacion && x.BultoID == bultoID).AsParallel().SingleOrDefault();
+                         bulto.Estatus = "Orden en proceso de recepcion";
+                         bulto.UsuarioModificacion = usuario.UsuarioID;
+                         bulto.FechaModificacion = DateTime.Now;
+
+                         ctx.SaveChanges();
+                     }
+                     else
+                     {
+                         Sam3_FolioCuantificacion cuantificacion = ctx.Sam3_FolioCuantificacion.Where(x => x.FolioCuantificacionID == FolioCuantificacion && x.FolioAvisoEntradaID == AvisoEntrada).AsParallel().SingleOrDefault();
+                         cuantificacion.Estatus = "Orden en proceso de recepcion";
+                         cuantificacion.UsuarioModificacion = usuario.UsuarioID;
+                         cuantificacion.FechaModificacion = DateTime.Now;
+
+                         ctx.SaveChanges();
+                     }
+
+                     TransactionalInformation result = new TransactionalInformation();
+                     result.ReturnMessage.Add("Ok");
+                     result.ReturnCode = 200;
+                     result.ReturnStatus = false;
+                     result.IsAuthenicated = true;
+
+                     return result;
+                 }
+             }
+             catch (Exception ex)
+             {
+                 TransactionalInformation result = new TransactionalInformation();
+                 result.ReturnMessage.Add(ex.Message);
+                 result.ReturnCode = 500;
+                 result.ReturnStatus = false;
+                 result.IsAuthenicated = true;
+
+                 return result;
+             }
+         }
+    }
+}
