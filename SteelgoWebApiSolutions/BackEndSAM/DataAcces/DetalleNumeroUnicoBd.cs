a8acbf44506dcca59cb0e40ae67beb8775748c10
danielaZC <daniela_pzc@hotmail.com>
2015-09-18 14:25:47 -0500

    informacion para detalle numero unico

---
+++ SteelgoWebApiSolutions/BackEndSAM/DataAcces/DetalleNumeroUnicoBd.cs
@@ -0,0 +1,108 @@
+﻿using BackEndSAM.Models;
+using DatabaseManager.Sam3;
+using SecurityManager.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSAM.DataAcces
+{
+    public class DetalleNumeroUnicoBd
+    {
+
+        private static readonly object _mutex = new object();
+        private static DetalleNumeroUnicoBd _instance;
+
+        /// <summary>
+        /// constructor privado para implementar el patron Singleton
+        /// </summary>
+        private DetalleNumeroUnicoBd()
+        {
+        }
+
+        /// <summary>
+        /// crea una instancia de la clase
+        /// </summary>
+        public static DetalleNumeroUnicoBd Instance
+        {
+            get
+            {
+                lock (_mutex)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new DetalleNumeroUnicoBd();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public object obtenerDatosInventario(string numeroUnicoID)
+        {
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+                    int sumaTotalEntradas = 0;
+                    int sumaTotalSalidas = 0;
+                    List<DetalleNumeroUnico> listado = new List<DetalleNumeroUnico>();
+                    List<int> totalEntradas = (from num in ctx.Sam3_NumeroUnicoMovimiento
+                                               join tm in ctx.Sam3_TipoMovimiento on num.TipoMovimientoID equals tm.TipoMovimientoID
+                                               where num.Activo && tm.Activo == 1 && tm.EsEntrada &&
+                                               num.NumeroUnicoID.ToString() == numeroUnicoID
+                                               select num.Cantidad).AsParallel().ToList();
+
+                    totalEntradas.ForEach(x => sumaTotalEntradas = sumaTotalEntradas + x);
+
+                    List<int> totalSalidas = (from num in ctx.Sam3_NumeroUnicoMovimiento
+                                              join tm in ctx.Sam3_TipoMovimiento on num.TipoMovimientoID equals tm.TipoMovimientoID
+                                              where num.Activo && tm.Activo == 1 && !tm.EsEntrada &&
+                                              num.NumeroUnicoID.ToString() == numeroUnicoID
+                                              select num.Cantidad).AsParallel().ToList();
+
+                    totalSalidas.ForEach(x => sumaTotalSalidas = sumaTotalSalidas + x);
+
+                    listado = (from nu in ctx.Sam3_NumeroUnico
+                               join ic in ctx.Sam3_ItemCode on nu.ItemCodeID equals ic.ItemCodeID
+                               join rics in ctx.Sam3_Rel_ItemCode_ItemCodeSteelgo on ic.ItemCodeID equals rics.ItemCodeID
+                               join ics in ctx.Sam3_ItemCodeSteelgo on rics.ItemCodeSteelgoID equals ics.ItemCodeSteelgoID
+                               join nui in ctx.Sam3_NumeroUnicoInventario on nu.NumeroUnicoID equals nui.NumeroUnicoID
+                               where nu.Activo && ic.Activo && rics.Activo && ics.Activo && nui.Activo
+                               && nu.NumeroUnicoID.ToString() == numeroUnicoID
+                               select new DetalleNumeroUnico
+                               {
+                                   NumeroUnico = nu.NumeroUnicoID.ToString(),
+                                   ItemCode = ic.Codigo,
+                                   ItemCodeSteelgo = ics.Codigo,
+                                   D1 = ics.Diametro1.ToString(),
+                                   D2 = ics.Diametro2.ToString(),
+                                   Cedula = ics.Cedula,
+                                   TotalRecibido = nui.CantidadRecibida.ToString(),
+                                   TotalDanado = nui.CantidadDanada.ToString(),
+                                   TotalEntradas = sumaTotalEntradas.ToString(),
+                                   TotalSalidas = sumaTotalSalidas.ToString(),
+                                   SaldoActual = nui.InventarioFisico.ToString()
+                               }).AsParallel().GroupBy(x=> x.NumeroUnico).Select(x=> x.First()).ToList();
+
+                    return listado;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+
+
+    }
+}
--- SteelgoWebApiSolutions/BackEndSAM/Models/ListadoMaterialesPorPL.cs
+++ SteelgoWebApiSolutions/BackEndSAM/Models/ListadoMaterialesPorPL.cs
@@ -23,4 +23,29 @@ namespace BackEndSAM.Models
         public string EstatusDocumental { get; set; }
         public string AlmacenVirtual { get; set; }
     }
+
+    public class DetalleNumeroUnico
+    {
+        public string NumeroUnico { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemCodeSteelgo { get; set; }
+        public string D1 { get; set; }
+        public string D2 { get; set; }
+        public string Cedula { get; set; }
+        public string TotalRecibido { get; set; }
+        public string TotalDanado { get; set; }
+        public string Profile1 { get; set; }
+        public string Profile2 { get; set; }
+        public string TotalEntradas { get; set; }
+        public string TotalSalidas { get; set; }
+        public string SaldoActual { get; set; }
+        public string Segmento { get; set; }
+        public string Rack { get; set; }
+        public string SalidasTemporales { get; set; }
+        public string Fecha { get; set; }
+        public string Movimiento { get; set; }
+        public string Entrada { get; set; }
+        public string Salida { get; set; }
+        public string Referencia { get; set; }
+    }
 }
