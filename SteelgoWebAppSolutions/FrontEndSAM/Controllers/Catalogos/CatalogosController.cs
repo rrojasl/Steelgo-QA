cf32a78229ba595c27f2c098e264ef0bb1c3640a
unknown <joaquin.ramos@DF-JRA-01.sieenasoftware.com>
2015-09-22 09:24:41 -0500

    Probando grid desde select

---
+++ SteelgoWebAppSolutions/FrontEndSAM/Controllers/Catalogos/CatalogosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEndSAM.Controllers.Catalogos
+{
+    public class CatalogosController : Controller
+    {
+        // GET: /AvisoLlegada/
+        public ActionResult ListadoCatalogos()
+        {
+            return View();
+        }
+    }
+}
