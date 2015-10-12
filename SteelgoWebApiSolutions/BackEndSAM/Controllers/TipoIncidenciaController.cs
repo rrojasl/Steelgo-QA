1270f2580779a174d5c3bca45c90e7268c5f2612
jhernandezt <julian.hernandez@definityfirst.com>
2015-10-12 14:04:53 -0500

    lista combo de incidencias

--- SteelgoWebApiSolutions/BackEndSAM/Controllers/TipoIncidenciaController.cs
+++ SteelgoWebApiSolutions/BackEndSAM/Controllers/TipoIncidenciaController.cs
@@ -41,6 +41,28 @@ namespace BackEndSAM.Controllers
             }
         }
 
+        public object Get(int tipoIncidencia, string busqueda, string token)
+        {
+            string payload = "";
+            string newToken = "";
+            bool tokenValido = ManageTokens.Instance.ValidateToken(token, out payload, out newToken);
+            if (tokenValido)
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                Sam3_Usuario usuario = serializer.Deserialize<Sam3_Usuario>(payload);
+                return ListadoBd.Instance.ListaComboIncidencia(tipoIncidencia, busqueda);
+            }
+            else
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(payload);
+                result.ReturnCode = 401;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = false;
+                return result;
+            }
+        }
+
         // GET api/tipoincidencia/5
         public IEnumerable<ListaCombos> Get(int tipoIncidenciaID, string token)
         {
