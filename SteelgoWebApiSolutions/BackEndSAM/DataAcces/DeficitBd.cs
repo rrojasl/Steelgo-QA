e90ab05a9a4e3474652adc7de1f7396720096f7a
danielaZC <daniela_pzc@hotmail.com>
2015-10-13 16:13:00 -0500

    grid spools en notificacion de deficit

--- SteelgoWebApiSolutions/BackEndSAM/DataAcces/DeficitBd.cs
+++ SteelgoWebApiSolutions/BackEndSAM/DataAcces/DeficitBd.cs
@@ -61,7 +61,7 @@ namespace BackEndSAM.DataAcces
                         proyectos = (from p in ctx.Sam3_Rel_Usuario_Proyecto
                                      join eqp in ctx.Sam3_EquivalenciaProyecto on p.ProyectoID equals eqp.Sam3_ProyectoID
                                      where p.Activo && eqp.Activo
-                                     && p.UsuarioID == 1 //usuario.UsuarioID
+                                     && p.UsuarioID == usuario.UsuarioID
                                      select eqp.Sam2_ProyectoID).Distinct().AsParallel().ToList();
 
                         proyectos = proyectos.Where(x => x > 0).ToList();
@@ -216,6 +216,81 @@ namespace BackEndSAM.DataAcces
                                           join itmc in ctx2.ItemCode on ms.ItemCodeID equals itmc.ItemCodeID
                                           where ot.OrdenTrabajoID.ToString() == ordenTrabajo && ms.ItemCodeID == itemCode
                                           select ms.Cantidad).Sum().ToString();
+                        });
+
+                        return lista;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //-----------------Agregar mensaje al Log -----------------------------------------------
+                LoggerBd.Instance.EscribirLog(ex);
+                //-----------------Agregar mensaje al Log -----------------------------------------------
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Funcion para obtener la informacion del grid de spools
+        /// Notificacion de deficit
+        /// </summary>
+        /// <param name="ordenTrabajo"></param>
+        /// <param name="itemCode"></param>
+        /// <returns></returns>
+        public object ObtenerInformacionSpools(int ordenTrabajo, int itemCode)
+        {
+            try 
+            {
+                using (SamContext ctx = new SamContext())
+                {
+                    using (Sam2Context ctx2 = new Sam2Context())
+                    {
+                        List<SpoolsDeficit> lista = (from ots in ctx2.OrdenTrabajoSpool
+                                                     join otm in ctx2.OrdenTrabajoMaterial on ots.OrdenTrabajoSpoolID equals otm.OrdenTrabajoSpoolID
+                                                     join ms in ctx2.MaterialSpool on otm.MaterialSpoolID equals ms.MaterialSpoolID
+                                                     join s in ctx2.Spool on ms.SpoolID equals s.SpoolID
+                                                     where ots.OrdenTrabajoID == ordenTrabajo && ms.ItemCodeID == itemCode
+                                                     select new SpoolsDeficit
+                                                     {
+                                                         SpoolID = s.SpoolID.ToString(),
+                                                         Spool = s.Nombre,
+                                                         Prioridad = s.Prioridad.ToString(),
+                                                         Peqs = s.PeqGrupo,
+                                                         Peso = s.Peso.ToString(),
+                                                         ItemCodeID = ms.ItemCodeID.ToString(),
+                                                     }).AsParallel().GroupBy(x => x.SpoolID).Select(x => x.First()).ToList();
+
+                        lista.ForEach(x =>
+                        {
+                            x.ItemCodes = (from eq in ctx.Sam3_EquivalenciaItemCode
+                                           join rics in ctx.Sam3_Rel_ItemCode_ItemCodeSteelgo on eq.Sam3_ItemCodeID equals rics.ItemCodeID
+                                           join ics in ctx.Sam3_ItemCodeSteelgo on rics.ItemCodeSteelgoID equals ics.ItemCodeSteelgoID
+                                           join ic in ctx.Sam3_ItemCode on eq.Sam3_ItemCodeID equals ic.ItemCodeID
+                                           where eq.Sam2_ItemCodeID.ToString() == x.ItemCodeID
+                                           select new Deficit
+                                           {
+                                               ItemCode = ic.Codigo,
+                                               ItemCodeID = ic.ItemCodeID.ToString(),
+                                               Diametro1 = ics.Diametro1.ToString(),
+                                               Diametro2 = ics.Diametro2.ToString()
+                                           }).AsParallel().ToList();
+                        });
+
+                        lista.ForEach(x =>
+                        {
+                            x.ItemCodes.ForEach(y =>
+                            {
+                                y.Cantidad = (from ms in ctx2.MaterialSpool
+                                              where ms.SpoolID.ToString() == x.SpoolID && ms.ItemCodeID == itemCode
+                                              select ms.Cantidad).Sum().ToString();
+                            });
                         });
 
                         return lista;
--- SteelgoWebApiSolutions/BackEndSAM/Models/Deficit.cs
+++ SteelgoWebApiSolutions/BackEndSAM/Models/Deficit.cs
@@ -19,4 +19,20 @@ namespace BackEndSAM.Models
         public string Diametro1 { get; set; }
         public string Diametro2 { get; set; }
     }
+
+    public class SpoolsDeficit
+    {
+        public string SpoolID { get; set; }
+        public string Spool { get; set; }
+        public string Prioridad { get; set; }
+        public string Peqs { get; set; }
+        public string Peso { get; set; }
+        public string ItemCodeID { get; set; }
+        public List<Deficit> ItemCodes { get; set; }
+
+        public SpoolsDeficit()
+        {
+            ItemCodes = new List<Deficit>();
+        }
+    }
 }
