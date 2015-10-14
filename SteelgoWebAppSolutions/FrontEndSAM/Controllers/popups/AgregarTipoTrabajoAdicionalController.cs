d7f223164b6bb8174071e28d0d8bd2f51a7e77ce
Eduardo Raul Martinez Dominguez <edu_09_11_91@hotmail.com>
2015-10-14 12:47:26 -0500

    Carpeta Popups a controllers

---
+++ SteelgoWebAppSolutions/FrontEndSAM/Controllers/popups/AgregarTipoTrabajoAdicionalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEndSAM.Controllers.popups
+{
+    public class AgregarTipoTrabajoAdicionalController : Controller
+    {
+        // GET: AgregarTipoTrabajoAdicional
+        public ActionResult creaTipoTrabajoAdicional()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ SteelgoWebAppSolutions/FrontEndSAM/Controllers/popups/AgregarTrabajoAdicionalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEndSAM.Controllers.popups
+{
+    public class AgregarTrabajoAdicionalController : Controller
+    {
+        // GET: AgregarTrabajoAdicional
+        public ActionResult creaTrabajoAdicional()
+        {
+            return PartialView();
+        }
+    }
+}
