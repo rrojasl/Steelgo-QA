bb3aa225243dbccb0fc7331f1a97fb58d83c218f
Jonathan Revilla <jrevillag@maftec.com.mx>
2015-10-21 18:00:37 -0500

    Reestructuración de código ListadoCamposPredeterminados

---
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Catalogos/ListadoCamposPredeterminados/Ajax.js
@@ -0,0 +1,14 @@
+﻿function AjaxObtenerListadoCamposPredeterminados(TipoDato) {
+    
+    $ListadoCamposPredeterminados.ListadoCamposPredeterminados.read({ TipoDato, token: Cookies.get("token") }).done(function (data) {
+        if (Error(data)) {
+            ResultadoJason = data;
+            if (ResultadoJason.length > 0) {
+                $("#gridCamposPredeterminados").data("kendoGrid").dataSource.data(ResultadoJason);
+            } else {
+                $("#gridCamposPredeterminados").data("kendoGrid").dataSource.data([]);
+            };
+        }
+    });
+
+}
