1f9bf965074c07b9f7b98970197130fedf1c0313
jemiresq <jonathan.miranda@definityfirst.com>
2015-10-29 13:29:10 -0600

    cambios en modelo de seguridad

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Core/SecurityManager.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Core/SecurityManager.js
@@ -230,6 +230,7 @@ function applySecurityPolicyForEntity(entityDefinition, entitySecurity, security
 
         if (entityDefinition["listContainer"].hasOwnProperty("destroy") && entityDefinition.listContainer["destroy"] != null && entityDefinition.listContainer["destroy"].length > 0) {
             if (entityDestroyPermission == false) {
+               //$(entityDefinition.listContainer["destroy"]).addClass("hidden");
                $(entityDefinition.listContainer["destroy"]).css("display", "none");
             }
         }
