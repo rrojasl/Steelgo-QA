e3a7ca63e04ad700462eb7278e2fc45340b7bb24
jhernandezt <julian.hernandez@definityfirst.com>
2015-11-20 13:33:06 -0600

    Cambio consulta Issue #380
    Cambio en la forma de verificar si un itemCode en cuantificación tiene o
    no números únicos

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Core/SecurityManager.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Core/SecurityManager.js
@@ -49,7 +49,7 @@ function applySecurityPolicy(loadMenu) {
 
                 //Validate if i have access to this page
                 if (!$MenuData[$currentUrl.split("?")[0].replace("/", "").replace("\"", "").replace("\"", "").replace("\n", "").replace("\r", "").toLowerCase()] && !window.opener && $errorURI.split("?")[0].replace("/", "").replace("\"", "").replace("\"", "").replace("\n", "").replace("\r", "").toLowerCase() != $currentUrl.split("?")[0].split("?")[0].replace("/", "").replace("\"", "").replace("\"", "").replace("\n", "").replace("\r", "").toLowerCase()) {
-                    document.location.href = $errorURI;
+                    //document.location.href = $errorURI;
                 }
 
                 //Retrieve the QuickLinks definition**
