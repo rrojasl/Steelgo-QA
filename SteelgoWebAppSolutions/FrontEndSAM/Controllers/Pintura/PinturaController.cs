b5f64aba5cbcfa52b69ae5a8b3725ad4b099b8f2
Sergio Ramírez Villa <sergio_ayax@hotmail.com>
2015-12-21 16:19:26 -0600

    Esqueleto Pintura

---
+++ SteelgoWebAppSolutions/FrontEndSAM/Controllers/Pintura/PinturaController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace FrontEndSAM.Controllers.Pintura
+{
+    public class PinturaController : Controller
+    {
+        public ActionResult CargaCarro()
+        {
+            return View();
+        }
+        public ActionResult CargaCarroBackLog()
+        {
+            return View();
+        }
+        public ActionResult CapturaAvance()
+        {
+            return View();
+        }
+        public ActionResult DescargaCarro()
+        {
+            return View();
+        }
+        public ActionResult CapturaAvanceIntAcabado()
+        {
+            return View();
+        }
+        public ActionResult LotesCapturaReporte()
+        {
+            return View();
+        }
+    }
+}
