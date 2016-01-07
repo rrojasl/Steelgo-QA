8de9ec09900264bb55d3a8e461279104c9848790
Eduardo Raul Martinez Dominguez <edu_09_11_91@hotmail.com>
2016-01-07 11:34:00 -0600

    Carpetas de orgnaizacion Pintura

---
+++ SteelgoWebApiSolutions/BackEndSAM/Controllers/PinturaControllers/CargaCarroBackLog/CargaCarroBackLogController.cs
@@ -0,0 +1,42 @@
+﻿using DatabaseManager.Sam3;
+using SecurityManager.Api.Models;
+using SecurityManager.TokenHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Script.Serialization;
+
+namespace BackEndSAM.Controllers.PinturaControllers.CargaCarroBackLog
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CargaCarroBackLogController : ApiController
+    {
+        [HttpGet]
+        public object ObtieneArea(string token)
+        {
+            string payload = "";
+            string newToken = "";
+            bool tokenValido = ManageTokens.Instance.ValidateToken(token, out payload, out newToken);
+            if (tokenValido)
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                Sam3_Usuario usuario = serializer.Deserialize<Sam3_Usuario>(payload);
+                return new object();
+            }
+            else
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(payload);
+                result.ReturnCode = 401;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = false;
+                return result;
+            }
+
+        }
+    }
+}
