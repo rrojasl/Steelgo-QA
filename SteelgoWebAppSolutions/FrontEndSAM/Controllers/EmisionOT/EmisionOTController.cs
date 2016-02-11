29f3e2461ddfbe4ed5b248c4ce1234efb4956d76
Lupita Hdez <lupita.hdez.rivera@hotmail.com>
2016-02-10 19:00:23 -0600

    Emision Ordenes de Trabajo

---
+++ SteelgoWebAppSolutions/FrontEndSAM/Controllers/EmisionOT/EmisionOTController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEndSAM.Controllers.Errors
+{
+    public class EmisionOTController : Controller
+    {
+        //
+        // GET: /Errors/
+        public ActionResult EmisionOT()
+        {
+            return View();
+        }
+    }
+}
