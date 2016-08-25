f4da377dc379899e8ce95391c106f033b7d6790b
Iris Cuevas <icuevas@maftec.com.mx>
2016-08-25 17:45:30 -0500

    Se crean archivos en BACKEND para "Entrega de Placas o Graficas"

---
+++ SteelgoWebApiSolutions/BackEndSAM/Controllers/ServiciosTecnicos/EntregaPlacasGraficas/EntregasPlacaGraficasController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace BackEndSAM.Controllers.ServiciosTecnicos.EntregaPlacasGraficas
+{
+    public class EntregasPlacaGraficasController : ApiController
+    {
+    }
+}
---
+++ SteelgoWebApiSolutions/BackEndSAM/Models/ServiciosTecnicos/EntregaPlacasGraficas/EntregaPlacasGraficas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSAM.Models.ServiciosTecnicos.EntregaPlacasGraficas
+{
+    public class EntregaPlacasGraficas
+    {
+    }
+}
