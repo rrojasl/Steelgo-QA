215d37a8a35e26207a01f7c1d9befd524cc35a71
Iris Cuevas <icuevas@maftec.com.mx>
2016-08-30 15:38:46 -0500

    Se genera clase Constantes para el guardado de los modulos desarrollados por Maftec Consulting y clase ObjetoSQL para de configuracion de conexión a la bd Sam3

---
+++ SteelgoWebApiSolutions/DatabaseManager/Constantes/Constantes.cs
@@ -0,0 +1,32 @@
+﻿namespace DatabaseManager.Constantes
+{
+    public static class Numeros
+    {
+        public static int CERO = 0;
+        public static int UNO = 1;
+        public static int DOS = 2;
+        public static int TRES = 3;
+        public static int CUATRO = 4;
+        public static int CINCO = 5;
+        public static int SEIS = 6;
+        public static int SIETE = 7;
+        public static int OCHO = 8;
+        public static int NUEVE = 9;
+        public static int DIES = 10;
+        public static int ONCE = 11;
+        public static int DOCE = 12;
+        public static int TRECE = 13;
+        public static int CATORCE = 14;
+        public static int QUINCE = 15;
+        public static int DIESISEIS = 16;
+        public static int DIESISIETE = 17;
+        public static int DIESIOCHO = 18;
+        public static int DIESINUEVE = 19;
+        public static int VEINTE = 20;
+    }
+
+    public static class Stords
+    {
+        public static string GUARDACAPTURAENTREGAPLACASGRAFICAS = "dbo.Sam3_ST_EPG_GuardaCaptura";
+    }
+}
---
+++ SteelgoWebApiSolutions/DatabaseManager/Sql/ObjetosSQL.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using DatabaseManager.Constantes;
+using System.Configuration;
+
+namespace DatabaseManager.Sam3
+{
+    public class ObjetosSQL
+    {
+        /// <summary>
+        /// Cadena Coneccion a la BD
+        /// </summary>
+        /// <returns></returns>
+        protected SqlConnection Conexion()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString);
+        }
+        /// <summary>
+        /// Retorna un DatatTable con la información de BD
+        /// </summary>
+        /// <param name="Stord">Stord a ejecutar</param>
+        /// <param name="Parametros">Parametros que requiere el stord</param>
+        /// <returns>Objeto DatatTable con la coleccion de datos</returns>
+        public DataTable Tabla(string Stord, string[,] Parametros = null)
+        {
+
+            DataTable dt = new DataTable();
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        cmd.CommandTimeout = 0;
+                        cmd.Connection.Open();
+                        da.Fill(dt);
+                        cmd.Connection.Close();
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+                    throw new Exception(e.Message);
+                }
+                return dt;
+            }
+
+        }
+        /// <summary>
+        /// Retorna un coleccion de Tablas con la información de BD
+        /// </summary>
+        /// <param name="Stord">nombre del stord a ejecutar</param>
+        /// <param name="Parametros">Parametros que requiere el stord</param>
+        /// <returns>Coleccion de tablas</returns>
+        public DataSet Coleccion(string Stord, string[,] Parametros = null)
+        {
+            DataSet ds = new DataSet();
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        cmd.CommandTimeout = 0;
+                        cmd.Connection.Open();
+                        da.Fill(ds);
+                        cmd.Connection.Close();
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+                    throw new Exception(e.Message);
+                }
+                return ds;
+            }
+
+
+        }
+        /// <summary>
+        /// Retorna un coleccion de Tablas con la información de BD
+        /// </summary>
+        /// <param name="Stord">Nombre del Stord a ejecutar</param>
+        /// <param name="TablaSube">objeto DataTable que envía al stord</param>
+        /// <param name="NombreTabla">nombre del parametro de tabla</param>
+        /// <param name="Parametros">Parametros que requiere el stord</param>
+        /// <returns>Coleccion de tablas</returns>
+        public DataSet Coleccion(string Stord, DataTable TablaSube, String NombreTabla, string[,] Parametros = null)
+        {
+            DataSet ds = new DataSet();
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.Parameters.Add(new SqlParameter(NombreTabla, TablaSube));
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        cmd.CommandTimeout = 0;
+                        cmd.Connection.Open();
+                        da.Fill(ds);
+                        cmd.Connection.Close();
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+                    throw new Exception(e.Message);
+                }
+                return ds;
+            }
+
+
+        }
+        /// <summary>
+        /// Ejecuta un stord en la BD
+        /// </summary>
+        /// <param name="Stord">Nombre del Stord a ejecutar</param>
+        /// <param name="Parametros">Parametros que requiere el stord</param>
+        /// <returns>True si se ejecuto el stord, Exception: error</returns>
+        public bool Ejecuta(string Stord, string[,] Parametros = null)
+        {
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    cmd.CommandTimeout = 0;
+                    cmd.Connection.Open();
+                    cmd.ExecuteNonQuery();
+                    cmd.Connection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ejecuta un stord en la BD
+        /// </summary>
+        /// <param name="Stord">Nombre del Stord a ejecutar</param>
+        /// <param name="Parametros">Parametros que requiere el stord</param>
+        /// <returns>regresa el numero de filas afectadas.</returns>
+        public int EjecutaStoreExecuteReader(string Stord, string[,] Parametros = null)
+        {
+            int rowsAfected = 0;
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    cmd.CommandTimeout = 0;
+                    cmd.Connection.Open();
+                    rowsAfected = cmd.ExecuteNonQuery();
+                    cmd.Connection.Close();
+                    return rowsAfected;
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+                    return rowsAfected;
+                }
+            }
+        }
+        /// <summary>
+        /// Ejecuta un stord en la BD
+        /// </summary>
+        /// <param name="Stord">Nombre del Stord a ejecutar</param>
+        /// <param name="TablaSube">objeto DataTable que envía al stord</param>
+        /// <param name="NombreTabla">nombre del parametro de tabla</param>
+        /// <param name="Parametros">Parametros que requiere el stord</param>
+        /// <returns>Objeto DatatTable con la coleccion de datos</returns>
+        public bool Ejecuta(string Stord, DataTable TablaSube, String NombreTabla, string[,] Parametros = null)
+        {
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.Parameters.Add(new SqlParameter(NombreTabla, TablaSube));
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    cmd.CommandTimeout = 0;
+                    cmd.Connection.Open();
+                    cmd.ExecuteNonQuery();
+                    cmd.Connection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+        public int EjecutaInsertUpdate(string Stord, DataTable TablaSube, String NombreTabla, string[,] Parametros = null)
+        {
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                int lastRow = 0;
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.Parameters.Add(new SqlParameter(NombreTabla, TablaSube));
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    cmd.CommandTimeout = 0;
+                    cmd.Connection.Open();
+                    lastRow = int.Parse(cmd.ExecuteScalar().ToString());
+                    cmd.Connection.Close();
+                    return lastRow;
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+        public DataTable Tabla(string Stord, DataTable TablaSube, String NombreTabla, string[,] Parametros = null)
+        {
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                DataTable dt = new DataTable();
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.Parameters.Add(new SqlParameter(NombreTabla, TablaSube));
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        cmd.CommandTimeout = 0;
+                        cmd.Connection.Open();
+                        da.Fill(dt);
+                        cmd.Connection.Close();
+                    }
+                    return dt;
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ejecuta un stord en la BD
+        /// </summary>
+        /// <param name="Stord">Nombre del Stord a ejecutar</param>
+        /// <param name="TablaSube">objeto DataTable que envía al stord</param>
+        /// <param name="NombreTabla">nombre del parametro de tabla</param>
+        /// <param name="Parametros">Parametros que requiere el stord</param>
+        /// <returns>Regresa un objeto Datatable</returns>
+        /// 
+        public DataTable EjecutaDataAdapter(string Stord, DataTable TablaSube, String NombreTabla, string[,] Parametros = null)
+        {
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                DataTable dt = new DataTable();
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.Parameters.Add(new SqlParameter(NombreTabla, TablaSube));
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        cmd.CommandTimeout = 0;
+                        cmd.Connection.Open();
+                        da.Fill(dt);
+                        cmd.Connection.Close();
+                    }
+
+
+                    return dt;
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+
+                    DataTable dtError = new DataTable("error");
+                    DataColumn dataColumn = null;
+                    for (int i = 0; i < 2; i++)
+                    {
+                        dataColumn = new DataColumn(i.ToString());
+                        dtError.Columns.Add(dataColumn);
+                    }
+
+                    DataRow row = dtError.NewRow();
+                    row["0"] = "error";
+                    row["1"] = "error";
+                    dtError.Rows.Add(row);
+                    return dtError;
+                }
+            }
+        }
+        /// <summary>
+        /// Ejecuta un stord en la BD
+        /// </summary>
+        /// <param name="Stord">Nombre del Stord a ejecutar</param>
+        /// <param name="TablaSube">objeto DataTable que envía al stord</param>
+        /// <param name="NombreTabla">nombre del parametro de tabla</param>
+        /// <param name="Parametros">Parametros que requiere el stord</param>
+        /// <returns>Objeto DatatTable con la coleccion de datos</returns>
+        public bool Ejecuta(string Stord, DataTable TablaSube, String NombreTabla, DataTable TablaSube1, String NombreTabla1, string[,] Parametros = null)
+        {
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.Parameters.Add(new SqlParameter(NombreTabla1, TablaSube1));
+                cmd.Parameters.Add(new SqlParameter(NombreTabla, TablaSube));
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    cmd.CommandTimeout = 0;
+                    cmd.Connection.Open();
+                    cmd.ExecuteNonQuery();
+                    cmd.Connection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+                    throw new Exception(e.Message);
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Ejecuta un stord en la BD
+        /// </summary>
+        /// <param name="Stord">Nombre del Stord a ejecutar</param>
+        /// <param name="TablaSube">objeto DataTable que envía al stord</param>
+        /// <param name="NombreTabla">nombre del parametro de tabla</param>
+        /// <param name="Parametros">Parametros que requiere el stord</param>
+        /// <returns>Objeto DatatTable con la coleccion de datos</returns>
+        public bool Ejecuta(string Stord, DataTable TablaSube, String NombreTabla, DataTable TablaSube1, String NombreTabla1, DataTable TablaSube2, String NombreTabla2, string[,] Parametros = null)
+        {
+
+            DataTable dt = new DataTable();
+            using (SqlCommand cmd = new SqlCommand(Stord, Conexion()))
+            {
+                if (Parametros != null)
+                    for (int i = Numeros.CERO; i < Parametros.Length / Numeros.DOS; i++)
+                        cmd.Parameters.AddWithValue(Parametros[i, Numeros.CERO].ToString(), Parametros[i, Numeros.UNO].ToString());
+                cmd.Parameters.Add(new SqlParameter(NombreTabla2, TablaSube2));
+                cmd.Parameters.Add(new SqlParameter(NombreTabla1, TablaSube1));
+                cmd.Parameters.Add(new SqlParameter(NombreTabla, TablaSube));
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    cmd.CommandTimeout = 0;
+                    cmd.Connection.Open();
+                    cmd.ExecuteNonQuery();
+                    cmd.Connection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    cmd.Connection.Close();
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+    }
+}
