776600e1529ee18d33229b18cf93b759743a2382
Iris Cuevas <icuevas@maftec.com.mx>
2016-09-06 20:28:08 -0500

    Se crea vista y scripts para la pantalla Editar Requisicion

---
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/PND/EditarRequisicion/Captura.js
@@ -0,0 +1,92 @@
+﻿IniciarEdicionRequisicion();
+function IniciarEdicionRequisicion() {
+    SuscribirEventos();
+}
+
+function changeLanguageCall() {
+    cargarGrid();
+    AjaxCargarCamposPredeterminados();
+    document.title = _dictionary.ServiciosTecnicosEntregaPGBreadcrumb[$("#language").data("kendoDropDownList").value()];
+}
+
+function cargarGrid() {
+    $("#grid").kendoGrid({
+        dataSource: {
+            schema: {
+                model: {
+                    fields: {
+                        NumeroControl: { type: "string", editable: false },
+                        EtiquetaJunta: { type: "string", editable: false },
+                        TipoJunta: { type: "string", editable: false },
+                        NombreRequisicion: { type: "string", editable: false },
+                        Cuadrante: { type: "string", editable: false },
+                        Prioridad: { type: "number", editable: false },
+                        Clasificacion: { type: "string", editable: false },
+                        Diametro: { type: "number", editable: false },
+                        Espesor: { type: "number", editable: false },
+                        Cedula: { type: "string", editable: false },
+                        RequisicionID: { type: "int", editable: false },
+                        ProyectoID: { type: "int", editable: false },
+                        SpoolID: { type: "int", editable: false },
+                        JuntaSpoolID: { type: "int", editable: false },
+                        OrdenTrabajoSpoolID: { type: "int", editable: false },
+                        JuntaSpoolID: { type: "int", editable: false },
+                        TipoPruebaID: { type: "int", editable: false },
+                        Especificacion: { type: "number", editable: false },
+                        Agregar: { type: "boolean", editable: false }
+                    }
+                }
+            },
+            filter: {
+                logic: "or",
+                filters: [
+                  { field: "Accion", operator: "eq", value: 1 },
+                  { field: "Accion", operator: "eq", value: 2 }
+                ]
+            },
+            pageSize: 10,
+            serverPaging: false,
+            serverFiltering: false,
+            serverSorting: false
+        },
+        navigatable: true,
+        editable: true,
+        autoHeight: true,
+        sortable: true,
+        scrollable: true,
+        selectable: true,
+        pageable: {
+            refresh: false,
+            pageSizes: [10, 25, 50, 100],
+            info: false,
+            input: false,
+            numeric: true
+        },
+        filterable: getGridFilterableMaftec(),
+        columns: [
+            { field: "NumeroControl", title: _dictionary.columnNumeroControl[$("#language").data("kendoDropDownList").value()], filterable: getGridFilterableCellMaftec(), width: "130px" },
+            { field: "EtiquetaJunta", title: _dictionary.columnJunta[$("#language").data("kendoDropDownList").value()], filterable: getGridFilterableCellMaftec(), width: "80px", attributes: { style: "text-align:right;" } },
+            { field: "TipoJunta", title: _dictionary.columnTipoJta[$("#language").data("kendoDropDownList").value()], filterable: getGridFilterableCellMaftec(), width: "112px" },
+            { field: "NombreRequisicion", title: _dictionary.columnRequisicion[$("#language").data("kendoDropDownList").value()], filterable: getGridFilterableCellMaftec(), width: "135px" },
+            { field: "Cuadrante", title: _dictionary.columnCuadrante[$("#language").data("kendoDropDownList").value()], filterable: getGridFilterableCellMaftec(), width: "127px" },
+            { field: "Prioridad", title: _dictionary.columnPrioridad[$("#language").data("kendoDropDownList").value()], filterable: getGridFilterableCellNumberMaftec(), width: "90px", attributes: { style: "text-align:right;" } },
+            { field: "Clasificacion", title: _dictionary.columnClasificacion[$("#language").data("kendoDropDownList").value()], filterable: getGridFilterableCellMaftec(), width: "85px" },
+            { field: "Diametro", title: _dictionary.columnDiametro[$("#language").data("kendoDropDownList").value()], filterable: getGridFilterableCellNumberMaftec(), width: "94px", attributes: { style: "text-align:right;" } },
+            { field: "Espesor", title: _dictionary.columnEspesor[$("#language").data("kendoDropDownList").value()], filterable: getGridFilterableCellNumberMaftec(), width: "112px", attributes: { style: "text-align:right;" } },
+            { field: "Cedula", title: _dictionary.columnCedula[$("#language").data("kendoDropDownList").value()], filterable: getGridFilterableCellMaftec(), width: "105px" },
+            {
+                field: "Agregar", title: _dictionary.columnAgregar[$("#language").data("kendoDropDownList").value()], filterable: {
+                    multi: true,
+                    messages: {
+                        isTrue: _dictionary.lblVerdadero[$("#language").data("kendoDropDownList").value()],
+                        isFalse: _dictionary.lblFalso[$("#language").data("kendoDropDownList").value()],
+                        style: "max-width:100px;"
+                    },
+                    dataSource: [{ Etiquetado: true }, { Etiquetado: false }]
+                }, template: "<input name='fullyPaid' class='ob-paid' type='checkbox' data-bind='checked: Agregar' #= Agregar ? checked='checked' : '' #/>", width: "112px", attributes: { style: "text-align:center;" }
+            },
+        ]
+    });
+
+    CustomisaGrid($("#grid"));
+}
---
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/PND/EditarRequisicion/Handlers.js
@@ -0,0 +1,47 @@
+﻿function SuscribirEventos() {
+    SuscribirEventoRequisicion();
+    SuscribirEventoProyecto();
+    SuscribirEventoTipoPrueba();
+}
+
+function SuscribirEventoRequisicion() {
+    $("#inputRequisicion").kendoComboBox({
+        dataTextField: "NombreRequisicion",
+        dataValueField: "RequisicionID",
+        suggest: true,
+        delay: 10,
+        filter: "contains",
+        index: 3,
+        change: function (e) {
+            var dataItem = this.dataItem(e.sender.selectedIndex);
+        }
+    });    
+}
+
+function SuscribirEventoProyecto() {
+    $("#inputProyecto").kendoComboBox({
+        dataTextField: "Proyecto",
+        dataValueField: "ProyectoID",
+        suggest: true,
+        delay: 10,
+        filter: "contains",
+        index: 3,
+        change: function (e) {
+            var dataItem = this.dataItem(e.sender.selectedIndex);
+        }
+    });
+}
+
+function SuscribirEventoTipoPrueba() {
+    $("#inputPrueba").kendoComboBox({
+        dataTextField: "TipoPrueba",
+        dataValueField: "TipoPruebaID",
+        suggest: true,
+        delay: 10,
+        filter: "contains",
+        index: 3,
+        change: function (e) {
+            var dataItem = this.dataItem(e.sender.selectedIndex);
+        }
+    });
+}
