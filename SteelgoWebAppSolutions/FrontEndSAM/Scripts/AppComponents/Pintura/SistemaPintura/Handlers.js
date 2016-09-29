563ebd0882f5ac1085747457c02c2fb5865187c3
Eduardo Raul Martinez Dominguez <edu_09_11_91@hotmail.com>
2016-09-29 16:48:34 -0500

    Se agrega vista de Sistema de Pintura, y correccion en los radio button, para el modo responsive de los modulos de RequisicionPND y Asignar Requisicion

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Pintura/SistemaPintura/Handlers.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Pintura/SistemaPintura/Handlers.js
@@ -1,1 +1,172 @@
-﻿
+﻿var proyectoInicial = 0;
+var pruebaOriginal = 0;
+var requisicionOriginal = 0;
+
+function SuscribirEventos() {
+    
+    suscribirEventoGuardar();
+    SuscribirEventoComboUnidadMedida();
+    suscribirEventoDetallePruebas();
+    SuscribirEventoCerrarPopUpPruebas();
+    suscribirEventoProyecto();
+};
+
+
+function suscribirEventoProyecto() {
+
+    $("#inputProyecto").kendoMultiSelect({
+        dataSource: '',
+        dataTextField: "Nombre",
+        dataValueField: "ProyectoID",
+        suggest: true,
+        filter: "contains",
+        change: function (e) {
+        }
+    }).data("kendoMultiSelect");
+    
+}
+
+
+function suscribirEventoDetallePruebas() {
+
+    $(document).on('click', '.EnlaceDetallePruebas', function (e) {
+        e.preventDefault();
+
+        if ($('#botonGuardar').text() == _dictionary.lblGuardar[$("#language").data("kendoDropDownList").value()]) {
+
+            var grid = $("#grid").data("kendoGrid"),
+            dataItem = grid.dataItem($(e.target).closest("tr"));
+            LlenarGridPopUp(dataItem);
+        }
+    });
+}
+
+function SuscribirEventoCerrarPopUpPruebas() {
+    $("#CerrarDetalleJunta").click(function (e) {
+        e.preventDefault();
+
+        $("#windowGrid").data("kendoWindow").close();
+    });
+}
+
+function suscribirEventoChangeRadio() {
+    $('input:radio[name=Muestra]:nth(0)').change(function () {
+        AjaxCargarRequisicionAsignacion();
+    });
+    $('input:radio[name=Muestra]:nth(1)').change(function () {
+        AjaxCargarRequisicionAsignacion();
+    });
+}
+
+
+function SuscribirEventoComboUnidadMedida() {
+    $('#inputUnidad').kendoComboBox({
+        dataTextField: "Nombre",
+        dataValueField: "UnidadMedidaID",
+        suggest: true,
+        filter: "contains",
+        index: 3,
+        change: function (e) {
+            dataItem = this.dataItem(e.sender.selectedIndex);
+            if (dataItem != undefined) {
+            }
+            else {
+                
+            }
+        }
+    });
+
+};
+
+
+function Limpiar() {
+    
+    $("#grid").data('kendoGrid').dataSource.data([]);
+}
+
+
+
+function suscribirEventoGuardar() {
+    $('#Guardar').click(function (e) {
+        e.preventDefault();
+        var ds = $("#grid").data("kendoGrid").dataSource;
+        if ($('#botonGuardar').text() == "Guardar") {
+            // opcionHabilitarView(true, "FieldSetView");
+            AjaxGuardarCaptura(ds._data, 0);
+        }
+        else if ($('#botonGuardar').text() == "Editar")
+            opcionHabilitarView(false, "FieldSetView")
+    });
+
+    $('#btnGuardar').click(function (e) {
+        e.preventDefault();
+        var ds = $("#grid").data("kendoGrid").dataSource;
+        if ($('#botonGuardar').text() == "Guardar") {
+            //opcionHabilitarView(true, "FieldSetView");
+            AjaxGuardarCaptura(ds._data, 0);
+        }
+        else if ($('#botonGuardar').text() == "Editar")
+            opcionHabilitarView(false, "FieldSetView")
+    });
+
+    $('#btnGuardarYNuevo').click(function (e) {
+        e.preventDefault();
+        var ds = $("#grid").data("kendoGrid").dataSource;
+        AjaxGuardarCaptura(ds._data, 1);
+        //Limpiar();
+    });
+
+
+    $('#GuardarPie').click(function (e) {
+        e.preventDefault();
+        var ds = $("#grid").data("kendoGrid").dataSource;
+        if ($('#botonGuardar').text() == "Guardar") {
+            // opcionHabilitarView(true, "FieldSetView");
+            AjaxGuardarCaptura(ds._data, 0);
+        }
+        else if ($('#botonGuardar').text() == "Editar")
+            opcionHabilitarView(false, "FieldSetView")
+    });
+
+    $('#btnGuardar1').click(function (e) {
+        e.preventDefault();
+        var ds = $("#grid").data("kendoGrid").dataSource;
+        if ($('#botonGuardar').text() == "Guardar") {
+            //opcionHabilitarView(true, "FieldSetView");
+            AjaxGuardarCaptura(ds._data, 0);
+        }
+        else if ($('#botonGuardar').text() == "Editar")
+            opcionHabilitarView(false, "FieldSetView")
+    });
+
+    $('#btnGuardarYNuevo1').click(function (e) {
+        e.preventDefault();
+        var ds = $("#grid").data("kendoGrid").dataSource;
+        AjaxGuardarCaptura(ds._data, 1);
+        //Limpiar();
+    });
+}
+
+function opcionHabilitarView(valor, name) {
+
+    if (valor) {
+        $('#FieldSetView').find('*').attr('disabled', true);
+        
+
+        $('#botonGuardar2').text(_dictionary.botonEditar[$("#language").data("kendoDropDownList").value()]);
+        $("#botonGuardar").text(_dictionary.botonEditar[$("#language").data("kendoDropDownList").value()]);
+        $("#botonGuardar3").text(_dictionary.botonEditar[$("#language").data("kendoDropDownList").value()]);
+        $('#botonGuardar4').text(_dictionary.botonEditar[$("#language").data("kendoDropDownList").value()]);
+
+    }
+    else {
+        $('#FieldSetView').find('*').attr('disabled', false);
+
+        $('#botonGuardar2').text(_dictionary.botonGuardar[$("#language").data("kendoDropDownList").value()]);
+        $("#botonGuardar").text(_dictionary.botonGuardar[$("#language").data("kendoDropDownList").value()]);
+        $("#botonGuardar3").text(_dictionary.botonGuardar[$("#language").data("kendoDropDownList").value()]);
+        $('#botonGuardar4').text(_dictionary.botonGuardar[$("#language").data("kendoDropDownList").value()]);
+
+
+    }
+}
