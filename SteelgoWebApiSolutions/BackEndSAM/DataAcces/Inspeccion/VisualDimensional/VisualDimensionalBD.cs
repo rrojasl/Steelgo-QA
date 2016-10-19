a28a3890077895fa914c260d0a18a153a2bd1620
MeryQuerido <mquerido@maftec.com.mx>
2016-10-18 20:38:38 -0500

    Back End Visual Dimensional

---
+++ SteelgoWebApiSolutions/BackEndSAM/DataAcces/Inspeccion/VisualDimensional/VisualDimensionalBD.cs
@@ -0,0 +1,325 @@
+﻿using BackEndSAM.Models.Inspeccion.VisualDimensional;
+using DatabaseManager.Constantes;
+using DatabaseManager.Sam3;
+using SecurityManager.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSAM.DataAcces.Inspeccion.VisualDimensionalBD
+{
+    public class VisualDimensionalBD
+    {
+        private static readonly object _mutex = new object();
+        private static VisualDimensionalBD _instance;
+
+        /// <summary>
+        /// constructor privado para implementar el patron Singleton
+        /// </summary>
+        private VisualDimensionalBD()
+        {
+        }
+
+        /// <summary>
+        /// crea una instancia de la clase
+        /// </summary>
+        public static VisualDimensionalBD Instance
+        {
+            get
+            {
+                lock (_mutex)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new VisualDimensionalBD();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Obtiene el detalle de la junta con respecto  la inspeccion visual dimensional por cada spool
+        /// </summary>
+        /// <param name="JsonCaptura"></param>
+        /// <param name="usuario"></param>
+        /// <param name="Lenguaje"></param>
+        /// <returns></returns>
+        public object ObtenerDetalleJunta(CapturaVisualDimensional JsonCaptura, Sam3_Usuario usuario, string Lenguaje, string juntasSeleccionadas)
+        {
+
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+
+                    List<Sam3_Inspeccion_Get_DetalleJunta_Result> listaDetalleDatosJson = ctx.Sam3_Inspeccion_Get_DetalleJunta(int.Parse(JsonCaptura.OrdenTrabajoSpoolID), Lenguaje).ToList();
+                    return listaDetalleDatosJson;
+                }
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Obtiene el detalle de la junta con respecto  la inspeccion
+        /// </summary>
+        /// <param name="JsonCaptura"></param>
+        /// <param name="usuario"></param>
+        /// <param name="Lenguaje"></param>
+        /// <returns></returns>
+        public object ObtenerDetalleJunta(int OrdenTrabajoSpoolID, Sam3_Usuario usuario, string Lenguaje)
+        {
+
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+
+                    List<Sam3_Steelgo_Get_JuntaSpool_Result> listaDetalleDatosJson = ctx.Sam3_Steelgo_Get_JuntaSpool(1, OrdenTrabajoSpoolID, null).ToList();
+                    return listaDetalleDatosJson;
+                }
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Obtiene el detalle de la junta con respecto  la inspeccion
+        /// </summary>
+        /// <param name="JsonCaptura"></param>
+        /// <param name="usuario"></param>
+        /// <param name="Lenguaje"></param>
+        /// <returns></returns>
+        public object ObtenerDetalleJunta(string OrdenTrabajoSpoolID, Sam3_Usuario usuario, string Lenguaje)
+        {
+
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+
+                    List<Sam3_Inspeccion_Get_DetalleJunta_Result> listaDetalleDatosJson = ctx.Sam3_Inspeccion_Get_DetalleJunta(int.Parse(OrdenTrabajoSpoolID), Lenguaje).ToList();
+                    return listaDetalleDatosJson;
+                }
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Obtiene las juntas con respecto  la inspeccion visual dimensional
+        /// </summary>
+        /// <param name="JsonCaptura"></param>
+        /// <param name="usuario"></param>
+        /// <param name="Lenguaje"></param>
+        /// <returns></returns>
+        public object ObtenerJuntasVisualDimensional(string OrdenTrabajoSpoolID, Sam3_Usuario usuario, string Lenguaje, int? todos)
+        {
+
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+
+                    List<Sam3_Inspeccion_VD_Get_JuntasXSpoolID_Result> listaDetalleDatosJson = ctx.Sam3_Inspeccion_VD_Get_JuntasXSpoolID(int.Parse(OrdenTrabajoSpoolID), Lenguaje, todos).ToList();
+                    return listaDetalleDatosJson;
+                }
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Obtiene el detalle de las juntas seleccionadas con respecto a  la inspeccion
+        /// </summary>
+        /// <param name="JsonCaptura"></param>
+        /// <param name="usuario"></param>
+        /// <param name="Lenguaje"></param>
+        /// <returns></returns>
+        public object ObtenerDetalleJuntaSeleccionada(string OrdenTrabajoSpoolID, int defectoID, Sam3_Usuario usuario, string Lenguaje)
+        {
+
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+
+                    List<int?> listaDetalleDatosSeleciconadoJson = ctx.Sam3_Inspeccion_Get_DetalleJuntaSeleccionada(int.Parse(OrdenTrabajoSpoolID), Lenguaje, defectoID).ToList();
+                    return listaDetalleDatosSeleciconadoJson;
+                }
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+
+
+        /// <summary>
+        /// Obtiene el detalle de las juntas seleccionadas con respecto a  la inspeccion visual dimensional
+        /// </summary>
+        /// <param name="JsonCaptura"></param>
+        /// <param name="usuario"></param>
+        /// <param name="Lenguaje"></param>
+        /// <returns></returns>
+        public object ObtenerDetalleJuntaSeleccionadaVisualDimensional(string OrdenTrabajoSpoolID, Sam3_Usuario usuario, string Lenguaje)
+        {
+
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+
+                    List<Sam3_Inspeccion_VD_Get_JuntasXSpoolID_Result> listaDetalleDatosSeleciconadoJson = ctx.Sam3_Inspeccion_VD_Get_JuntasXSpoolID(int.Parse(OrdenTrabajoSpoolID), Lenguaje, 3).ToList();
+                    return listaDetalleDatosSeleciconadoJson;
+                }
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+        /// <summary>
+        /// Retorna el listado de numeros unicos ¿
+        /// </summary>
+        /// <param name="JsonCaptura"></param>
+        /// <param name="usuario"></param>
+        /// <returns></returns>
+        public object listaNumeroUnicos(int JuntaSpoolID, Sam3_Usuario usuario, int pagina)
+        {
+
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+                    List<Sam3_Armado_Get_MaterialesSpool_Result> listaDetallaTrabajoAdicionalJson = ctx.Sam3_Armado_Get_MaterialesSpool(JuntaSpoolID, 1, pagina).ToList();
+                    return listaDetallaTrabajoAdicionalJson;
+                }
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+
+        public object ObtenerJuntasXSpoolID(Sam3_Usuario usuario, string ordenTrabajo, string id, int sinCaptura)
+        {
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+                    List<Sam3_Steelgo_Get_JuntaSpool_Result> lista = ctx.Sam3_Steelgo_Get_JuntaSpool(sinCaptura, int.Parse(id), 1).ToList();
+                    return lista.OrderBy(x => int.Parse(x.Etiqueta)).ToList<Sam3_Steelgo_Get_JuntaSpool_Result>();
+                }
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+                return result;
+            }
+        }
+
+        public object InsertarCapturaInspeccion(DataTable dtDetalleCaptura, DataTable dtJuntaLista, int usuario, string lenguaje, int inspeccionDimensionalID, int ordenTrabajoSpoolID, string fechaInspeccion, int resultadoID, int obreroID, int defectoID, int accion)
+        {
+            try
+            {
+                using (SamContext ctx = new SamContext())
+                {
+
+                    // ctx.Sam3_Armado_JuntaArmado()
+                    ObjetosSQL _SQL = new ObjetosSQL();
+                    string[,] parametro = {
+                        { "@Usuario", usuario.ToString() },
+                        { "@Lenguaje", lenguaje },
+                        { "@InspeccionDimensionalID", inspeccionDimensionalID.ToString() },
+                        { "@OrdenTrabajoSpoolID", ordenTrabajoSpoolID.ToString() },
+                        { "@FechaInspeccion", fechaInspeccion == null ? "" : fechaInspeccion },
+                        { "@ResultadoID", resultadoID.ToString() },
+                        { "@ObreroID", obreroID.ToString() },
+                        { "@DefectoID", defectoID.ToString() },
+                        { "@AccionDimensional", accion.ToString() }
+
+                    };
+                    _SQL.Ejecuta(Stords.GUARDARCAPTURAINSPECCIONVISUALDIMENSIONAL, dtDetalleCaptura, "@Inspeccion", dtJuntaLista, "@juntas", parametro);
+
+
+                    TransactionalInformation result = new TransactionalInformation();
+                    result.ReturnMessage.Add("Ok");
+
+                    result.ReturnCode = 200;
+                    result.ReturnStatus = true;
+                    result.IsAuthenicated = true;
+
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(ex.Message);
+                result.ReturnCode = 500;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = true;
+
+                return result;
+            }
+        }
+    }
+}
---
+++ SteelgoWebApiSolutions/BackEndSAM/Models/Inspeccion/VisualDimensional/VisualDimensional.cs
@@ -0,0 +1,218 @@
+﻿using BackEndSAM.Models.Inspeccion.Dimensional;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSAM.Models.Inspeccion.VisualDimensional
+{
+    public class CapturaVisualDimensional
+    {
+        public string EtiquetaMaterial1 { get; set; }
+        public string EtiquetaMaterial2 { get; set; }
+        public int Accion { get; set; }
+        public int DetalleArmadoID { get; set; }
+        public int JuntaArmadoID { get; set; }
+        public int InspeccionVisualID { get; set; }
+        public int ProyectoID { get; set; }
+        public string Proyecto { get; set; }
+        public string OrdenTrabajoID { get; set; }
+        public string OrdenTrabajo { get; set; }
+        public string OrdenTrabajoSpoolID { get; set; }
+        public string OrdenTrabajoSpool { get; set; }
+        public string SpoolID { get; set; }
+        public int JuntaID { get; set; }
+        public string Junta { get; set; }
+        public string TipoJunta { get; set; }
+        public string TipoJuntaID { get; set; }
+        public string Diametro { get; set; }
+        public string Resultado { get; set; }
+        public string ResultadoID { get; set; }
+        public string TallerID { get; set; }
+        public string Taller { get; set; }
+        public List<Taller> ListaTaller { get; set; }
+        public string DefectosID { get; set; }
+        public string Defectos { get; set; }
+        public List<Defectos> ListaDefectos { get; set; }
+        public string InspectorID { get; set; }
+        public string Inspector { get; set; }
+        public List<Inspector> ListaInspector { get; set; }
+        public string FechaInspeccion { get; set; }
+
+        public string NumeroUnico1ID { get; set; }
+        public string NumeroUnico2ID { get; set; }
+
+        public string NumeroUnico1 { get; set; }
+        public string NumeroUnico2 { get; set; }
+        public List<NumeroUnico> ListaNumerosUnicos1 { get; set; }
+        public List<NumeroUnico> ListaNumerosUnicos2 { get; set; }
+        public List<Resultado> ListaResultados { get; set; }
+
+        public string DetalleJunta { get; set; }
+
+    }
+    public class Inspector
+    {
+        public Inspector()
+        {
+            ObreroID = 0;
+            Codigo = "";
+            NombreCompleto = "";
+        }
+        public int ObreroID { get; set; }
+        public string Codigo { get; set; }
+        public string NombreCompleto { get; set; }
+    }
+    public class Defectos
+    {
+        public int DefectoID { get; set; }
+        public string Nombre { get; set; }
+        public int IDDEFECTOTIPO { get; set; }
+        public string TIPO { get; set; }
+
+        public Defectos()
+        {
+            DefectoID = 0;
+            Nombre = "";
+            IDDEFECTOTIPO = 0;
+            TIPO = "NoEspecificarJunta";
+        }
+    }
+    public class NumeroUnico
+    {
+        public int NumeroUnicoID { get; set; }
+        public string Clave { get; set; }
+        public int EtiquetaMaterial { get; set; }
+        public string Etiqueta { get; set; }
+        public string JuntasEncontradas { get; set; }
+
+        public NumeroUnico()
+        {
+            NumeroUnicoID = 0;
+            Clave = "";
+            EtiquetaMaterial = 0;
+            Etiqueta = "";
+            JuntasEncontradas = "";
+        }
+    }
+    public class Taller
+    {
+        public int TallerID { get; set; }
+
+        public string Nombre { get; set; }
+    }
+    public class Resultado
+    {
+        public string _ResultadoID { get; set; }
+        public string _Resultado { get; set; }
+    }
+
+    public class DetalleCapturaInspeccion
+    {
+        public string _ResultadoID { get; set; }
+        public string _Resultado { get; set; }
+    }
+
+
+    public class DetalleJuntaDimension
+    {
+        //public List<JuntaXSpoolID> ListaJuntaXSpoolID { get; set; }
+
+        public List<DetalleDimensional> ListaDetalleDimensional { get; set; }
+    }
+
+    public class JuntaXSpoolID
+    {
+        public string Etiqueta { get; set; }
+        public int JuntaSpoolID { get; set; }
+    }
+
+    public class JuntaXSpoolIDModeloJunta
+    {
+        public string Junta { get; set; }
+        public int JuntaID { get; set; }
+    }
+
+    public class DetalleDimensional
+    {
+        public int InspeccionDimensionalID { get; set; }
+
+        public int OrdenTrabajoSpoolID { get; set; }
+
+        public string FechaInspeccion { get; set; }
+
+        public int ResultadoID { get; set; }
+
+        public string Resultado { get; set; }
+
+        public int ObreroID { get; set; }
+
+        public string Inspector { get; set; }
+
+        public int DefectoID { get; set; }
+
+        public string Defecto { get; set; }
+        public List<InspeccionDimensional.JuntaXSpool> ListaJuntas { get; set; }
+        public List<InspeccionDimensional.JuntaXSpool> ListaJuntasSeleccionadas { get; set; }
+        public List<InspeccionDimensional.JuntaXSpool> ListaJuntasInicial { get; set; }
+    }
+
+    public class Captura
+    {
+        public List<DetalleGuardarJson> Detalles { get; set; }
+    }
+
+
+
+    public class DetalleGuardarJson
+    {
+        public int Accion { get; set; }
+        public string Lenguaje { get; set; }
+        public int InspeccionDimensionalID { get; set; }
+        public int OrdenTrabajoSpoolID { get; set; }
+        public string FechaInspeccion { get; set; }
+        public int ResultadoID { get; set; }
+        public int ObreroID { get; set; }
+        public int DefectoID { get; set; }
+
+        public List<DetalleGuardarInspeccionVisual> ListaDetalleGuardarInspeccionVisual { get; set; }
+
+        public List<InspeccionDimensional.JuntaXSpoolGuardar> ListaJuntas { get; set; }
+    }
+
+    public class DetalleGuardarInspeccionVisual
+    {
+        public int Accion { get; set; }
+
+        public int OrdenTrabajoSpoolID { get; set; }
+        public int TipoJuntaID { get; set; }
+
+        public string EtiquetaJunta { get; set; }
+        public int EtiquetaMaterial1 { get; set; }
+        public int EtiquetaMaterial2 { get; set; }
+
+        public string DefectosID { get; set; }
+
+        public string ObreroID { get; set; }
+        public string FechaInspeccion { get; set; }
+
+        public int DetalleArmadoID { get; set; }
+
+        public string ResultadoID { get; set; }
+        public string TallerID { get; set; }
+
+        public string NumeroUnico1ID { get; set; }
+        public string NumeroUnico2ID { get; set; }
+
+        public int InspeccionVisualID { get; set; }
+    }
+    public class InspeccionCamposPredeterminados
+    {
+        public string FechaDimensional { get; set; }
+        public string FechaVisual { get; set; }
+        public string ResultadoDimensional { get; set; }
+        public string ResultadoVisual { get; set; }
+        public string FormatoFecha { get; set; }
+
+    }
+}
