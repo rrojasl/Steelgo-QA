a07e4323ff86f9061663f8c57beaac3fb1582436
Iris Cuevas <icuevas@maftec.com.mx>
2016-10-25 17:34:26 -0500

    Se agrega seguridad a la pantalla

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Pintura/SistemaPinturaApicable/Seguridad.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Pintura/SistemaPinturaApicable/Seguridad.js
@@ -1,1 +1,28 @@
-﻿
+﻿Cookies.set("home", true, { path: '/' });
+Cookies.set("navegacion", "10033", { path: '/' });
+
+var $SistemaPinturaAplicableModel = {
+    listContainer: {
+        create: "",
+        list: "",
+        detail: "",
+        destroy: ""
+    },
+    properties: {
+        inputProyecto: {
+            visible: "#divProyecto",
+            editable: "#inputProyecto",
+            required: "#inputProyecto",
+        },
+        inputSpool: {
+            visible: "#divSpool",
+            editable: "#inputSpool",
+            required: "#inputSpool",
+        },
+        inputNumeroControl: {
+            visible: "#divNc",
+            editable: "#inputNc",
+            required: "#inputNc",
+        }
+    }
+};
