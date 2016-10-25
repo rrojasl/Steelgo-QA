12495b5aaaeff60a5b396908e9718cb4dfff2f02
MeryQuerido <mquerido@maftec.com.mx>
2016-10-24 19:33:49 -0500

    Cambios a WPS

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/ConfiguracionSoldadura/WPS/Ajax.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/ConfiguracionSoldadura/WPS/Ajax.js
@@ -1,16 +1,16 @@
 ﻿function ObtenerJSONParaGrid() {
     loadingStart();
-    //$WPS.WPS.read({ TipoDato: 1, token: Cookies.get("token") }).done(function (data) {
-    //    if (Error(data)) {
-    //        $("#grid").data('kendoGrid').dataSource.data([]);
-    //        var ds = $("#grid").data("kendoGrid").dataSource;
-    //        var array = data;
-    //        for (var i = 0; i < array.length; i++) {
-    //            ds.add(array[i]);
-    //        }
-    //    }
-    //    loadingStop();
-    //});
+    $WPS.WPS.read({ TipoDato: 1, token: Cookies.get("token") }).done(function (data) {
+        if (Error(data)) {
+            $("#grid").data('kendoGrid').dataSource.data([]);
+            var ds = $("#grid").data("kendoGrid").dataSource;
+            var array = data;
+            for (var i = 0; i < array.length; i++) {
+                ds.add(array[i]);
+            }
+        }
+        loadingStop();
+    });
 };
 
 
