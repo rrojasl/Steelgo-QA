7dc6b4dbd6073e497dbe2ef2a878ab7ce10a0f92
MeryQuerido <mquerido@maftec.com.mx>
2016-10-25 19:04:39 -0500

    Se añade seguridad al módulo OK PND

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/PND/OKPND/Seguridad.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/PND/OKPND/Seguridad.js
@@ -1,1 +1,18 @@
-﻿
+﻿Cookies.set("home", true, { path: '/' });
+Cookies.set("navegacion", "10036", { path: '/' });
+
+var $OKPNDMODEL = {
+    listContainer: {
+        create: "",
+        list: "",
+        detail: "",
+        destroy: ""
+    },
+    properties: {
+        inputProyecto: {
+            visible: "#ProyectoDiv",
+            editable: "#Proyecto",
+            required: "#Proyecto",
+        }
+    }
+};
