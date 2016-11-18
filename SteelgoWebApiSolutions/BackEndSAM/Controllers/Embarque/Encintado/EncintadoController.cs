5269b5c9f6ee14aa390535f28f24e68f475d2a0d
Eduardo Raul Martinez Dominguez <edu_09_11_91@hotmail.com>
2016-11-18 11:00:13 -0600

    Se agrega backend a Encintado

---
+++ SteelgoWebApiSolutions/BackEndSAM/Controllers/Embarque/Encintado/EncintadoController.cs
@@ -0,0 +1,108 @@
+﻿using BackEndSAM.DataAcces.Embarque.Encintado;
+using BackEndSAM.Models.Embarque.Encintado;
+using DatabaseManager.Sam3;
+using SecurityManager.Api.Models;
+using SecurityManager.TokenHandler;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Script.Serialization;
+
+namespace BackEndSAM.Controllers.Embarque.Encintado
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class EncintadoController : ApiController
+    {
+        [HttpGet]
+        public object ObtieneDetalleEtiquetado(string token, int TipoConsulta, int Todos, int ZonaID, int CuadranteID, string SpoolContiene)
+        {
+            string payload = "";
+            string newToken = "";
+
+            bool tokenValido = ManageTokens.Instance.ValidateToken(token, out payload, out newToken);
+
+            if (tokenValido)
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                Sam3_Usuario usuario = serializer.Deserialize<Sam3_Usuario>(payload);
+                if (TipoConsulta == 1)
+                {
+                    return EncintadoBD.Instance.ObtieneDetalleEncintadoPorZona(ZonaID, CuadranteID, Todos);
+                }
+                else
+                {
+                    return EncintadoBD.Instance.ObtieneDetalleEncintadoPorSpool(SpoolContiene, Todos);
+                }
+
+            }
+            else
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(payload);
+                result.ReturnCode = 401;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = false;
+
+                return result;
+            }
+        }
+
+        [HttpGet]
+        public object ObtieneRutaSpool(string token, int SpoolID, int TipoReporte)
+        {
+            string payload = "";
+            string newToken = "";
+
+            bool tokenValido = ManageTokens.Instance.ValidateToken(token, out payload, out newToken);
+
+            if (tokenValido)
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                Sam3_Usuario usuario = serializer.Deserialize<Sam3_Usuario>(payload);
+                return EncintadoBD.Instance.ObtieneRutaSpool(SpoolID, TipoReporte);
+
+            }
+            else
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(payload);
+                result.ReturnCode = 401;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = false;
+
+                return result;
+            }
+        }
+
+        [HttpPost]
+        public object GuardaCapturaEtiquetado(CapturaEncintado captura, string token, string lenguaje)
+        {
+            string payload = "";
+            string newToken = "";
+
+            bool tokenValidado = ManageTokens.Instance.ValidateToken(token, out payload, out newToken);
+            if (tokenValidado)
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                Sam3_Usuario usuario = serializer.Deserialize<Sam3_Usuario>(payload);
+                DataTable dtDetalle = Utilities.ConvertirDataTable.ToDataTable.Instance.toDataTable(captura.listaDetalle);
+                return EncintadoBD.Instance.GuardaCapturaEncintado(dtDetalle, usuario.UsuarioID, lenguaje);
+            }
+            else
+            {
+                TransactionalInformation result = new TransactionalInformation();
+                result.ReturnMessage.Add(payload);
+                result.ReturnCode = 401;
+                result.ReturnStatus = false;
+                result.IsAuthenicated = false;
+
+                return result;
+            }
+        }
+    }
+}
---
+++ SteelgoWebApiSolutions/BackEndSAM/Models/Embarque/Encintado/Encintado.cs
@@ -0,0 +1,51 @@
+﻿using BackEndSAM.Models.Sam3General.Cuadrante;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndSAM.Models.Embarque.Encintado
+{
+    public class DetalleEncintado
+    {
+        public int Accion { get; set; }
+        public int SpoolID { get; set; }
+        public string Spool { get; set; }
+        public int ProyectoID { get; set; }
+        public string Proyecto { get; set; }
+        public int CuadranteID { get; set; }
+        public string Cuadrante { get; set; }
+        public int EncintadoID { get; set; }
+        public bool Encintado { get; set; }
+        public bool ModificadoPorUsuario { get; set; }
+        public bool OkPintura { get; set; }
+        public bool OkPnd { get; set; }
+        public int ZonaID { get; set; }
+        public string Zona { get; set; }
+        public List<UbicacionCuadrante> ListaCuadrantes { get; set; }
+    }
+
+    public class DetalleGuardadoEncintado
+    {
+        public int Accion { get; set; }
+        public int SpoolID { get; set; }
+        public int ColorID { get; set; }
+        public int CuadranteID { get; set; }
+        public int Encintado { get; set; }
+    }
+
+    public class DetalleRutaSpool
+    {
+        public int SpoolID { get; set; }
+        public string Spool { get; set; }
+        public int ProyectoID { get; set; }
+        public string Proyecto { get; set; }
+        public string Ruta { get; set; }
+    }
+
+    public class CapturaEncintado
+    {
+        public List<DetalleGuardadoEncintado> listaDetalle { get; set; }
+    }
+
+}
