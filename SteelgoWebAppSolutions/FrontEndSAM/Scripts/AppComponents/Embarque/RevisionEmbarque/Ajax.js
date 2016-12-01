d54fe4114fb57fe0a153db104e71c5f5c18de444
Eduardo Raul Martinez Dominguez <edu_09_11_91@hotmail.com>
2016-11-30 18:56:02 -0600

    Primeros cambios en Revision embarque

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Embarque/RevisionEmbarque/Ajax.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Embarque/RevisionEmbarque/Ajax.js
@@ -1,1 +1,24 @@
-﻿
+﻿function AjaxCargarProyecto() {
+    loadingStart();
+    $Proyectos.Proyectos.read({ token: Cookies.get("token") }).done(function (data) {
+        if (Error(data)) {
+            $("#Proyecto").data("kendoComboBox").dataSource.data([]);
+            var proyectoId = 0;
+
+            if (data.length > 0) {
+                $("#Proyecto").data("kendoComboBox").dataSource.data(data);
+                if (data.length < 3) {
+                    for (var i = 0; i < data.length; i++) {
+                        if (data[i].ProyectoID != 0) {
+                            proyectoId = data[i].ProyectoID;
+                        }
+                    }
+                }
+
+                $("#Proyecto").data("kendoComboBox").value(proyectoId);
+                $("#Proyecto").data("kendoComboBox").trigger("change");
+            }
+        }
+        loadingStop();
+    });
+}
