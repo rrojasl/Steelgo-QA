2f7aaf23d6f8f7a2779607d30bf79447b1e67a10
Iris Cuevas <icuevas@maftec.com.mx>
2016-12-22 18:37:48 -0600

    Seguridad para Preparacion de embarque

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Embarque/Embarque/Seguridad.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Embarque/Embarque/Seguridad.js
@@ -1,1 +1,38 @@
-﻿
+﻿Cookies.set("home", true, { path: '/' });
+Cookies.set("navegacion", "10020", { path: '/' });
+
+var $EmbarqueModel = {
+    listContainer: {
+        create: "",
+        list: "",
+        detail: "",
+        destroy: ""
+    },
+    properties: {
+        Proyecto: {
+            visible: "#ProyectoDiv",
+            editable: "#Proyecto",
+            required: "#Proyecto",
+        },
+        Proveedor: {
+            visible: "#ProveedorDiv",
+            editable: "#Proveedor",
+            required: "#Proveedor",
+        },
+        Tracto: {
+            visible: "#TractoDiv",
+            editable: "#Tracto",
+            required: "#Tracto",
+        },
+        Chofer: {
+            visible: "#ChoferDiv",
+            editable: "#Chofer",
+            required: "#Chofer",
+        },
+        Embarque: {
+            visible: "#EmbarqueDiv",
+            editable: "#Embarque",
+            required: "#Embarque",
+        }
+    }
+};
