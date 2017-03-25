4fc9d7d02e9e7c68b7f1081458c661961e05b3be
Ruben Rojas <rrojasl@maftec.com.mx>
2017-03-24 18:18:45 -0600

    Seguridad carga carro

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Pintura/AdminCargaCarro/Seguridad.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Pintura/AdminCargaCarro/Seguridad.js
@@ -1,1 +1,31 @@
 ﻿
+Cookies.set("home", true, { path: '/' });
+Cookies.set("navegacion", "10032", { path: '/' });
+
+var $AdminCargaCarroModel = {
+    listContainer: {
+        create: "",
+        list: "",
+        detail: "",
+        destroy: ""
+    },
+    properties: {
+        inputProyecto: {
+            visible: "#DivProyecto",
+            editable: "#inputProyecto",
+            required: "#inputProyecto"
+        },
+        inputCarro: {
+            visible: "#DivCarro",
+            editable: "#inputCarro",
+            required: "#inputCarro"
+        },
+        InputID: {
+            visible: "#InputIDDiv",
+            editable: "#InputID",
+            required: "#InputID",
+        }
+
+        
+    }
+};
